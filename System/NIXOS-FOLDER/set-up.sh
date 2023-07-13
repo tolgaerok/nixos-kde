6e9af0104371c0c561ec2053206ae4e73362efa1
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-13 23:58:47 +0800

    Create set-up.sh

---
+++ System/NIXOS-FOLDER/set-up.sh
@@ -0,0 +1,19 @@
+
+
+sudo mkdir -p /mnt/Budget-Archives
+sudo mkdir -p /mnt/home-profiles
+sudo mkdir -p /mnt/linux-data
+sudo mkdir -p /mnt/smb
+sudo mkdir -p /mnt/smb-budget
+sudo mkdir -p /mnt/smb-rsync
+sudo mkdir -p /mnt/windows-data
+sudo chmod 777 /mnt/Budget-Archives
+sudo chmod 777 /mnt/home-profiles
+sudo chmod 777 /mnt/linux-data
+sudo chmod 777 /mnt/smb
+sudo chmod 777 /mnt/smb-budget
+sudo chmod 777 /mnt/smb-rsync
+sudo chmod 777 /mnt/windows-data
+
+sudo mkdir -p /home/NixOs-KDE
+sudo chmod 0757 /home/NixOs-KDE
