b554d3df95bb7b905ca9932d1787d2a2e22a03fa
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-14 00:06:49 +0800

    Update set-up.sh

--- System/NIXOS-FOLDER/set-up.sh
+++ System/NIXOS-FOLDER/set-up.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
 
+# Tolga Erok
+# 14/7/2023
 
+sudo NIXPKGS_ALLOW_UNFREE=1 nix-shell -p hplipWithPlugin --run 'sudo -E hp-setup'
+sudo smbpasswd -a tolga && sudo groupadd samba && sudo usermod -aG samba tolga
 sudo mkdir -p /mnt/Budget-Archives
 sudo mkdir -p /mnt/home-profiles
 sudo mkdir -p /mnt/linux-data
@@ -14,6 +19,11 @@ sudo chmod 777 /mnt/smb
 sudo chmod 777 /mnt/smb-budget
 sudo chmod 777 /mnt/smb-rsync
 sudo chmod 777 /mnt/windows-data
-
 sudo mkdir -p /home/NixOs-KDE
 sudo chmod 0757 /home/NixOs-KDE
+
+# run after sudo nixos-rebuild switch
+sudo nix-channel --update
+sudo nix-env --upgrade
+sudo nixos-rebuild switch
+sudo nix-store --optimise
