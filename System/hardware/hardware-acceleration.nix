073fbab5141e7338507baafa499be306ab825fd8
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-15 15:31:38 +0800

    Update hardware-acceleration.nix

--- System/hardware/hardware-acceleration.nix
+++ System/hardware/hardware-acceleration.nix
@@ -9,11 +9,11 @@ in {
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      intel-media-driver   # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel           # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       vaapiVdpau
       libvdpau-va-gl
-      # nvidia_x11          # Nvidia X11 driver for GT1030
+      # nvidia_x11         # Nvidia X11 driver for GT1030
     ];
   };
 }
