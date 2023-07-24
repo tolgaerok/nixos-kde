fab5edc9c42ac9ccaf3972769241e694c0031c2d
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-24 11:53:52 +0800

    Update system-bash-aliases.nix

--- nixos/nix/aliases/system-bash-aliases.nix
+++ nixos/nix/aliases/system-bash-aliases.nix
@@ -11,7 +11,7 @@
 
       # Add any custom bash shell or aliases here
       shellAliases = {
-        garbage =     "sudo nix-collect-garbage --delete-older-than 14d";
+        garbage =     "sudo nix-collect-garbage --delete-older-than 7d";
         htos =        "sudo ~/scripts/MYTOOLS/scripts/Zysnc-Options/ZYSNC-HOME-TO-SERVER.sh";
         lgens =       "sudo nix-env --profile /nix/var/nix/profiles/system --list-generations";
         master =      "sudo ~/scripts/MYTOOLS/main.sh";
@@ -21,10 +21,29 @@
         neu =         "sudo nix-env --upgrade";
         nopt =        "sudo nix-store --optimise";
         rbs =         "sudo nixos-rebuild switch";
-        rebuild-all = "sudo nix-collect-garbage --delete-older-than 14d && sudo nixos-rebuild switch";
+        rebuild-all = "sudo nix-collect-garbage --delete-older-than 7d && sudo nixos-rebuild switch";
         stoh =        "sudo ~/scripts/MYTOOLS/scripts/Zysnc-Options/ZYSNC-SERVER-TO-HOME.sh";
         trimgen =     "sudo ~/scripts/MYTOOLS/scripts/GENERATION-TRIMMER/TrimmGenerations.sh";
         umount =      "sudo ~/scripts/MYTOOLS/scripts/Mounting-Options/UMOUNT-ALL.sh";
+        
+        # navigate files and directories
+        cd = "cd ..";
+        ls = "lsd";
+        ll = "lsd -l";
+        la = "lsd -a";
+        lsla = "lsd -la";
+        cl = "clear";
+        copy = "rsync -P";
+
+        # fun stuff
+        icons = "sxiv -t /home/tolga/Pictures/icons";
+        wp = "sxiv -t /home/tolga/Pictures/Wallpapers";
+
+        # file access
+        cp = "cp -riv";
+        mv = "mv -iv";
+        mkdir = "mkdir -vp";
+
       };
     };
   };
