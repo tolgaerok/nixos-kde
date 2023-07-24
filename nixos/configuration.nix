0c0fae76356325c1c13dd6c7e53e8536767811a5
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-24 21:48:55 +0800

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -177,9 +177,19 @@ in {
   # (https://davidturcotte.com)
   #---------------------------------------------------------------------
 
-  hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-  hardware.opengl.enable = true;
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+  };
+
   services.xserver.videoDrivers = [ "nvidia" ];
 
   #---------------------------------------------------------------------
