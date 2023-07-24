876b48db0f7a9e703f0d03a985b646cd5ee00187
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-24 22:53:17 +0800

    Update default.nix

--- nixos/hardware-acceleration/default.nix
+++ nixos/hardware-acceleration/default.nix
@@ -6,14 +6,14 @@ in {
     vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
     # nvidia_x11 = pkgs.nvidia_x11.override { enableGeforce1030 = true; };
   };
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
-      # nvidia_x11        # Nvidia X11 driver for GT1030
-    ];
-  };
+  # hardware.opengl = {
+  #   enable = true;
+  #   extraPackages = with pkgs; [
+  #     intel-media-driver # LIBVA_DRIVER_NAME=iHD
+  #     vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+  #     vaapiVdpau
+  #     libvdpau-va-gl
+  #     # nvidia_x11        # Nvidia X11 driver for GT1030
+  #   ];
+  # };
 }
