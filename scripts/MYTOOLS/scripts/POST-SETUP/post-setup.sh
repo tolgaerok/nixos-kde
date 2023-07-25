063a0283e57051cc19a7155d7ffb857a48f26a2d
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-07-25 20:14:25 +0800

    Update post-setup.sh

--- scripts/MYTOOLS/scripts/POST-SETUP/post-setup.sh
+++ scripts/MYTOOLS/scripts/POST-SETUP/post-setup.sh
@@ -14,6 +14,37 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # -----------------------------------------------------------------------------------
+# Set some variables & functions
+# -----------------------------------------------------------------------------------
+
+# Location of private samba folder
+shared_folder="/home/NixOs-KDE"
+
+# Get the user and group names using the IDs
+user_name=$(id -un "$user_id")
+group_name=$(getent group "$group_id" | cut -d: -f1)
+
+# Function to create directories if they don't exist and set permissions
+create_directory_if_not_exist() {
+  if [ ! -d "$1" ]; then
+    mkdir -p "$1"
+    echo "Created directory: $1"
+    chmod 757 "$1"    # Set full access (read, write, and execute) for user, group, and others
+  fi
+}
+
+# Function to check and update permissions of existing directories
+update_directory_permissions() {
+  if [ -d "$1" ]; then
+    perm=$(stat -c "%a" "$1")
+    if [ "$perm" != "757" ]; then
+      echo "Updating permissions of existing directory: $1"
+      chmod 757 "$1"
+    fi
+  fi
+}
+
+# -----------------------------------------------------------------------------------
 # Get user id an group id
 # -----------------------------------------------------------------------------------
 
@@ -21,42 +52,57 @@ fi
 if [ "$(id -u)" = "0" ] && [ -n "$SUDO_USER" ]; then
   # Get the original non-root user ID and group ID
   user_id=$(id -u "$SUDO_USER")
-  group_id=$(id -g "$SUDO_USER")
-
-  # Get the user and group names using the IDs
-  user_name=$(id -un "$user_id")
-  group_name=$(getent group "$group_id" | cut -d: -f1)
+  group_id=$(id -g "$SUDO_USER") 
 
   # Display the non-root user ID, name, and group ID
   echo "Non-Root User: $user_name (ID: $user_id)"
   echo "Non-Root Group: $group_name (ID: $group_id)"
 else
   echo "This script should be run using sudo."
+  exit 1
 fi
 
-#username=$(id -u -n 1000)
-#builddir=$(pwd)
-
 # -----------------------------------------------------------------------------------
-#  Create some directories
+#  Create some directories and set permissions
 # -----------------------------------------------------------------------------------
 
-sudo -u tolga mkdir -p /home/tolga/Pictures
-sudo -u tolga mkdir -p /home/tolga/Videos
-sudo -u tolga mkdir -p /home/tolga/Music
-sudo -u tolga mkdir -p /home/tolga/Documents
-sudo mkdir -p ~/.config/nix
-echo "experimental-features  = nix-command flakes" >> ~/.config/nix/nix.conf
+if [ -n "$user_name" ] && [ -n "$group_name" ]; then
+  home_dir="/home/$user_name"
+  config_dir="$home_dir/.config/nix"
+
+  create_directory_if_not_exist "$home_dir"
+  create_directory_if_not_exist "$config_dir"
+
+  # Update directory permissions
+  update_directory_permissions "$home_dir/Documents"
+  update_directory_permissions "$home_dir/Music"
+  update_directory_permissions "$home_dir/Pictures"
+  update_directory_permissions "$home_dir/Public"
+  update_directory_permissions "$home_dir/Templates"
+  update_directory_permissions "$home_dir/Videos"
+
+  # Set ownership for directories
+  sudo chown -R "$user_name":"$group_name" "$home_dir"
+
+  # Additional chown commands for directories
+  create_directory_if_not_exist "$home_dir/Documents"
+  create_directory_if_not_exist "$home_dir/Music"
+  create_directory_if_not_exist "$home_dir/Pictures"
+  create_directory_if_not_exist "$home_dir/Public"
+  create_directory_if_not_exist "$home_dir/Templates"
+  create_directory_if_not_exist "$home_dir/Videos"
+
+  # Give full permissions to the nix.conf file
+  echo "experimental-features  = nix-command flakes" | sudo -u "$user_name" tee "$config_dir/nix.conf"
+  chmod 757 "$config_dir/nix.conf"    # Set full access (read and write) for user, group, and others
+else
+  echo "Failed to retrieve non-root user and group information."
+  exit 1
+fi
 
 # -----------------------------------------------------------------------------------
-# Set directories permissions to username and groupname
+# Flatpak section
 # -----------------------------------------------------------------------------------
-
-sudo chown $user_name:$group_name /home/tolga/Pictures
-sudo chown $user_name:$group_name /home/tolga/Videos
-sudo chown $user_name:$group_name /home/tolga/Music
-sudo chown $user_name:$group_name /home/tolga/Documents
-
 
 echo "Install Flatpak apps..."
 
@@ -92,126 +138,72 @@ sudo flatpak uninstall --unused --delete-data -y
 
 echo "Software install complete..."
 
-sudo NIXPKGS_ALLOW_UNFREE=1 nix-shell -p hplipWithPlugin --run 'sudo -E hp-setup'
+# -----------------------------------------------------------------------------------
+#  Create some SMB user and group
+# -----------------------------------------------------------------------------------
 
-echo "Install samba and create user/group"
+# Function to read user input and prompt for input
+prompt_input() {
+  read -p "$1" value
+  echo "$value"
+}
 
-# Prompt for the desired username for samba
-read -p $'\n'"Enter the USERNAME to add to Samba: " sambausername
+# Create user/group
+echo "Time to create smb user & group"
 
-# Prompt for the desired name for samba
-read -p $'\n'"Enter the GROUP name to add username to Samba: " sambagroup
+# Prompt for the desired username and group for Samba
+sambausername=$(prompt_input $'\nEnter the USERNAME to add to Samba: ')
+sambagroup=$(prompt_input $'\nEnter the GROUP name to add username to Samba: ')
 
-sudo groupadd $sambagroup
-sudo useradd -m $sambausername
-sudo smbpasswd -a $sambausername
-sudo usermod -aG $sambagroup $sambausername
+# Create Samba user and group
+sudo groupadd "$sambagroup"
+sudo useradd -m "$sambausername"
+sudo smbpasswd -a "$sambausername"
+sudo usermod -aG "$sambagroup" "$sambausername"
 
-echo "
-Continuing..."
+# Pause and continue
+echo -e "\nContinuing..."
 read -r -n 1 -s -t 1
 
-sudo smbpasswd -a tolga 
-sudo groupadd samba 
-sudo usermod -aG samba tolga
+# Create and configure the shared folder
 
-sudo mkdir -p /home/NixOs-KDE
-sudo chgrp samba /home/NixOs-KDE
-sudo chmod 0757 /home/NixOs-KDE
+sudo mkdir -p "$shared_folder"
+sudo chgrp samba "$shared_folder"
+sudo chmod 0757 "$shared_folder"
 
-read -r -p "
-Continuing..." -t 1 -n 1 -s
+# Pause and continue
+read -r -p "Continuing..." -t 1 -n 1 -s
 
-# Configure usershares plugin
-read -r -p "Create and configure the Samba Filesharing Plugin ...
-" -t 3 -n 1 -s
+# Configure Samba Filesharing Plugin for a user
+echo -e "\nCreate and configure the Samba Filesharing Plugin..."
 
-# Prompt for the desired username
-read -p $'\n'"Enter the username to configure Samba Filesharing Plugin for: " username
+# Prompt for the desired username to configure Samba Filesharing Plugin
+username=$(prompt_input $'\nEnter the username to configure Samba Filesharing Plugin for: ')
 
 # Set umask value
 umask 0002
 
-# Set the shared folder path
-shared_folder="/home/$username"
-
 # Set permissions for the shared folder and parent directories (excluding hidden files and .cache directory)
-find "$shared_folder" -type d ! -path '/.' ! -path '/.cache' -exec chmod 0757 {}
-2>/dev/null
+find "$shared_folder" -type d ! -path '/.' ! -path '/.cache' -exec chmod 0757 {} \; 2>/dev/null
 
 # Create the sambashares group if it doesn't exist
 sudo groupadd -r sambashares
 
 # Create the usershares directory and set permissions
 sudo mkdir -p /var/lib/samba/usershares
-sudo chown $username:sambashares /var/lib/samba/usershares
+sudo chown "$username:sambashares" /var/lib/samba/usershares
 sudo chmod 1770 /var/lib/samba/usershares
 
 # Restore SELinux context for the usershares directory
 sudo restorecon -R /var/lib/samba/usershares
 
 # Add the user to the sambashares group
-sudo gpasswd sambashares -a $username
-
-# Add the user to the sambashares group (alternative method)
-sudo usermod -aG sambashares $username
+sudo gpasswd sambashares -a "$username"
 
 # Set permissions for the user's home directory
-sudo chmod 0757 /home/$username
+sudo chmod 0757 "/home/$username"
 
-# Path to alias script
-main_script="./setup-alias.sh"
-
-# Check if the main script exists
-if [[ -e "$main_script" ]]; then
-  # Execute the main script using source
-  source "$main_script"
-else
-  echo "Main script not found: $main_script"
-fi
-
-
-# run after sudo nixos-rebuild switch
+# Run the following commands after sudo nixos-rebuild switch
 sudo nix-channel --update
 sudo nixos-rebuild switch
 sudo nix-store --optimise
-
-# Not too sure ones:
-#sudo nix-env --upgrade
-#sudo mkdir -p /mnt/Budget-Archives
-#sudo mkdir -p /mnt/home-profiles
-#sudo mkdir -p /mnt/linux-data
-#sudo mkdir -p /mnt/smb
-#sudo mkdir -p /mnt/smb-budget
-#sudo mkdir -p /mnt/smb-rsync
-#sudo mkdir -p /mnt/windows-data
-#sudo chmod 777 /mnt/Budget-Archives
-#sudo chmod 777 /mnt/home-profiles
-#sudo chmod 777 /mnt/linux-data
-#sudo chmod 777 /mnt/smb
-#sudo chmod 777 /mnt/smb-budget
-#sudo chmod 777 /mnt/smb-rsync
-#sudo chmod 777 /mnt/windows-data
-
-source #HOME/.bashrc
-#alias mynix='sudo ~/scripts/MYTOOLS/scripts/COMMAN-NIX-COMMAND-SCRIPT/MyNixOS-commands.sh'
-#alias mount='sudo ~/scripts/MYTOOLS/scripts/Mounting-Options/MOUNT-ALL.sh'
-#alias umount='sudo ~/scripts/MYTOOLS/scripts/Mounting-Options/UMOUNT-ALL.sh'
-#alias mse='sudo ~/scripts/MYTOOLS/MAKE-SCRIPTS-EXECUTABLE.sh'
-#alias htos='sudo ~/scripts/MYTOOLS/scripts/Zysnc-Options/ZYSNC-HOME-TO-SERVER.sh'
-#alias stoh='sudo ~/scripts/MYTOOLS/scripts/Zysnc-Options/ZYSNC-SERVER-TO-HOME.sh'
-#{ config, pkgs, ... }:
-
-
-# {
-#  users.users.root = {
-#    shell = pkgs.mkShell {
- #     buildInputs = [ pkgs.bashInteractive ];
- #     shellHook = ''
-#        echo "Setting up root shell environment"
-#        echo "alias tolga='sudo /home/tolga/scripts/MYTOOLS/scripts/COMMAN-NIX-COMMAND-SCRIPT/MyNixOS-commands.sh'" >> $HOME/.profile
-#      '';
-#    };
-#  };
-# }
-
