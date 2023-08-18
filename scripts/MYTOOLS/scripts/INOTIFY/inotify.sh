fd9a4936864bb380362aaa22657ca75ce3b71963
tolgaerok <kingtolga@gmail.com>
2023-08-18 22:59:40 +0800

    Code Update

--- scripts/MYTOOLS/scripts/INOTIFY/inotify.sh
+++ scripts/MYTOOLS/scripts/INOTIFY/inotify.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
-# Set your source and destination paths
-SOURCE_DIR="/home/tolga/nixos/nixos/"
-DEST_REPO="https://github.com/tolgaerok/nixos.git"
+
+# Tolga Erok 
+# 18/8/23
+
+# Set source and destination paths
+SOURCE_DIR="/home/tolga/Templates/"
+DEST_REPO="/home/tolga/nixos/nixos/"
+
+# Function to handle git operations and push changes
+commit_and_push() {
+    # Get the current date and time
+    current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
+    # Add all changed files to the staging area
+    git add .
+
+    # Commit changes with date and time
+    git commit -m "Automated changes: $filename - $current_datetime"
+
+    # Push the changes to GitHub (adjust the remote and branch as needed)
+    git remote set-url origin "$DEST_REPO"
+    git push origin main
+}
 
 # Start inotifywait to monitor changes
-inotifywait -m -r -e modify,create,delete "$SOURCE_DIR" |
-    while read -r directory events filename; do
-        # Copy the changed file to the local repo
-        cp "$SOURCE_DIR/$filename" "$DEST_REPO/$filename"
+while true; do
+    inotifywait -e modify,create,delete -r "$SOURCE_DIR"
+    
+    # Remove the index lock file if it exists
+    rm -f "$DEST_REPO/.git/index.lock"
 
-        # Navigate to the repo directory
-        cd "$DEST_REPO"
-
-        # Get the current date and time
-        current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
-
-        # Add and commit the changes with date and time
-        git add "$filename"
-        git commit -m "Automated changes: $filename - $current_datetime"
-
-        # Push the changes to GitHub (adjust the remote and branch as needed)
-        git push origin main
+    # Wait for any existing Git process to finish
+    while [[ -f "$DEST_REPO/.git/index.lock" ]]; do
+        sleep 1
     done
+    
+    # Navigate to the repo directory or exit if navigation fails
+    cd "$DEST_REPO" || exit
+
+    # Prompt for automatic staging and committing
+    read -r -p "Automatic staging and committing? (y/n): " choice < /dev/tty
+    if [[ "$choice" == [Yy] ]]; then
+        # Copy the changed files to the local repo
+        cp -r "$SOURCE_DIR" "$DEST_REPO"
+        filename="Multiple files"
+        commit_and_push
+    else
+        echo "Changes saved. You can commit and push later."
+    fi
+done
