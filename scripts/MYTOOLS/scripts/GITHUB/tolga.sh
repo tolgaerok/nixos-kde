5561765efdcfdb3dbe1bcbd873ae863607f9b05e
tolgaerok <kingtolga@gmail.com>
2023-08-20 23:46:21 +0800

    Update at 11:46 PM

--- scripts/MYTOOLS/scripts/GITHUB/tolga.sh
+++ scripts/MYTOOLS/scripts/GITHUB/tolga.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Tolga Erok
-# ¯\_(ツ)_/¯
-
+# Personal nixos git folder uploader!
+# Tolga Erok. ¯\_(ツ)_/¯
 # 20/8/23
 
-# My nixos configuration file location
 config_files=(
     "$HOME/nixos/"
 )
@@ -26,7 +24,7 @@ else
     echo "If you haven't already, generate an SSH key pair:"
     echo "ssh-keygen -t ed25519 -C 'your email'"
     echo "Add your SSH key to the agent:"
-    echo "eval $(ssh-agent -s)"
+    echo "eval \$(ssh-agent -s)"
     echo "ssh-add ~/.ssh/id_ed25519"
     echo "Then, add your SSH public key to your GitHub account:"
     echo "cat ~/.ssh/id_ed25519.pub"
@@ -36,8 +34,7 @@ else
     exit 1
 fi
 
-git_dir="$HOME/shiva/shiva.git"
-work_tree="$HOME/shiva"
+work_tree="$HOME/nixos"
 
 # Navigate to the working tree directory
 cd "$work_tree" || exit
@@ -52,16 +49,17 @@ for path in "${config_files[@]}"; do
 done
 
 commit_time=$(date +"%I:%M %p") # 12-hour format
-git commit -m "update $(date) at $commit_time"
+git commit -m "Update at $commit_time"
 echo "Committed local changes"
 
-# Handle file deletions
+# Commit changes from deletions
 git add --all
-commit_time=$(date +"%I:%M %p") # Update commit time
 git commit -m "Edited commit @ $commit_time"
 echo "Committed edits"
 
 # Push changes to remote
-push_time=$(date +"%I:%M %p") # Update push time
 git push origin main
-echo "Pushed changes to remote repository at $push_time"
+echo "Pushed changes to remote repository at $commit_time"
+
+# Display Global settings
+git config --global --list
