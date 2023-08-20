472017f2d815432d4b94ba5ebc5962a94f87de6a
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-08-20 13:52:23 +0800

    Update tolga.sh
    Finally got my cli github working

--- scripts/MYTOOLS/scripts/GITHUB/tolga.sh
+++ scripts/MYTOOLS/scripts/GITHUB/tolga.sh
@@ -1,20 +1,67 @@
 #!/bin/bash
 
 # Tolga Erok
-# git remote set-url origin git@github.com:tolgaerok/nixos.git
+# ¯\_(ツ)_/¯
 
+# 20/8/23
 
+# My nixos configuration file location
 config_files=(
-    ~/nixos/nixos/
+    "$HOME/nixos/"
 )
 
-git_dir="$HOME/nixos/nixos/.git"
-work_tree="$HOME"
+# Check if the remote URL is set to SSH
+remote_url=$(git remote get-url origin)
 
+# Add some tweaks
+git config --global core.compression 9
+git config --global core.deltaBaseCacheLimit 2g
+git config --global diff.algorithm histogram
+git config --global http.postBuffer 524288000
 
-for file in "${config_files[@]}"; do
-    git --git-dir="$git_dir" --work-tree="$work_tree" add "$file"
+if [[ $remote_url == *"git@github.com"* ]]; then
+    echo "Remote URL is set to SSH. Proceeding with the script..."
+else
+    echo "Remote URL is not set to SSH. Please set up SSH key-based authentication for the remote repository."
+    echo "If you haven't already, generate an SSH key pair:"
+    echo "ssh-keygen -t ed25519 -C 'your email'"
+    echo "Add your SSH key to the agent:"
+    echo "eval $(ssh-agent -s)"
+    echo "ssh-add ~/.ssh/id_ed25519"
+    echo "Then, add your SSH public key to your GitHub account:"
+    echo "cat ~/.ssh/id_ed25519.pub"
+    echo "Finally, update your Git configuration to use SSH:"
+    echo "git config --global credential.helper store"
+    echo "Remote URL needs to be updated to SSH. Exiting..."
+    exit 1
+fi
+
+git_dir="$HOME/shiva/shiva.git"
+work_tree="$HOME/shiva"
+
+# Navigate to the working tree directory
+cd "$work_tree" || exit
+
+# Pull remote changes using merge
+git pull origin main --no-rebase
+echo "Pulled remote changes using merge"
+
+# Add and commit local changes
+for path in "${config_files[@]}"; do
+    git add "$path"
 done
 
-git --git-dir="$git_dir" --work-tree="$work_tree" commit -m "update $(date)"
-git --git-dir="$git_dir" --work-tree="$work_tree" push
+commit_time=$(date +"%I:%M %p") # 12-hour format
+git commit -m "update $(date) at $commit_time"
+echo "Committed local changes"
+
+# Handle file deletions
+git add --all
+commit_time=$(date +"%I:%M %p") # Update commit time
+git commit -m "Edited commit @ $commit_time"
+echo "Committed edits"
+
+# Push changes to remote
+push_time=$(date +"%I:%M %p") # Update push time
+git push origin main
+echo "Pushed changes to remote repository at $push_time"
