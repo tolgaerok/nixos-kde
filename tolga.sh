d0238b456183a12e8e9b30cc90f416eeed0445d3
tolgaerok <kingtolga@gmail.com>
2023-08-20 22:50:08 +0800

    update Sun 20 Aug 2023 22:50:08 AWST at 10:50 PM

--- tolga.sh
+++ tolga.sh
@@ -1,55 +1,50 @@
 #!/bin/bash
 
-        # Tolga Erok
-        # ¯\_(ツ)_/¯
+# Tolga Erok
+# ¯\_(ツ)_/¯
 
-        # 20/8/23
+# 20/8/23
 
-        # My nixos configuration file location
-        config_files=(
-            "$HOME/nixos/"
-        )
+# ¯\_(ツ)_/¯ Random test
+config_files=(
+    "$HOME/nixos/"
+)
 
-        # Check if the remote URL is set to SSH
-        remote_url=$(git remote get-url origin)
+# Check if the remote URL is set to SSH
+remote_url=$(git remote get-url origin)
 
-        # Rebase to reconcile divergent branches
-        git pull origin main --rebase
+# Add some tweaks
+git config --global core.compression 9
+git config --global core.deltaBaseCacheLimit 2g
+git config --global diff.algorithm histogram
+git config --global http.postBuffer 524288000
 
-        # Add some tweaks
-        git config --global core.compression 9
-        git config --global core.deltaBaseCacheLimit 2g
-        git config --global diff.algorithm histogram
-        git config --global http.postBuffer 524288000
+if [[ $remote_url == *"git@github.com"* ]]; then
+    echo "Remote URL is set to SSH. Proceeding with the script..."
+else
+    echo "Remote URL is not set to SSH. Please set up SSH key-based authentication for the remote repository."
+    echo "If you haven't already, generate an SSH key pair:"
+    echo "ssh-keygen -t ed25519 -C 'your email'"
+    echo "Add your SSH key to the agent:"
+    echo "eval $(ssh-agent -s)"
+    echo "ssh-add ~/.ssh/id_ed25519"
+    echo "Then, add your SSH public key to your GitHub account:"
+    echo "cat ~/.ssh/id_ed25519.pub"
+    echo "Finally, update your Git configuration to use SSH:"
+    echo "git config --global credential.helper store"
+    echo "Remote URL needs to be updated to SSH. Exiting..."
+    exit 1
+fi
 
-        if [[ $remote_url == *"git@github.com"* ]]; then
-            echo "Remote URL is set to SSH. Proceeding with the script..."
-        else
-            echo "Remote URL is not set to SSH. Please set up SSH key-based authentication for the remote repository."
-            echo "If you haven't already, generate an SSH key pair:"
-            echo "ssh-keygen -t ed25519 -C 'your email'"
-            echo "Add your SSH key to the agent:"
-            echo "eval $(ssh-agent -s)"
-            echo "ssh-add ~/.ssh/id_ed25519"
-            echo "Then, add your SSH public key to your GitHub account:"
-            echo "cat ~/.ssh/id_ed25519.pub"
-            echo "Finally, update your Git configuration to use SSH:"
-            echo "git config --global credential.helper store"
-            echo "Remote URL needs to be updated to SSH. Exiting..."
-            exit 1
-        fi
+git_dir="$HOME/shiva/shiva.git"
+work_tree="$HOME/nixos"
 
-        # git_dir="$HOME/nixos/nixos.git"
-        work_tree="$HOME/nixos"
+# Navigate to the working tree directory
+cd "$work_tree" || exit
 
-        # Navigate to the working tree directory
-        cd "$work_tree" || exit
-
-        # Pull remote changes using merge
-        git pull origin main --no-rebase
-        echo "Pulled remote changes using merge"
-
-       # ... (previous script lines)
+# Pull remote changes using merge
+git pull origin main --no-rebase
+echo "Pulled remote changes using merge"
 
 # Add and commit local changes
 for path in "${config_files[@]}"; do
@@ -61,10 +56,7 @@ git commit -m "update $(date) at $commit_time"
 echo "Committed local changes"
 
 # Handle file deletions
-for path in "${config_files[@]}"; do
-    git add --all "$path"
-done
-
+git add --all
 commit_time=$(date +"%I:%M %p") # Update commit time
 git commit -m "Edited commit @ $commit_time"
 echo "Committed edits"
@@ -73,5 +65,3 @@ echo "Committed edits"
 push_time=$(date +"%I:%M %p") # Update push time
 git push origin main
 echo "Pushed changes to remote repository at $push_time"
-
-
