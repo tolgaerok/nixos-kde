45c3bddf99ed396d08951dcea98f74fd4d0ed257
tolgaerok <kingtolga@gmail.com>
2023-08-20 23:17:18 +0800

    update script

--- custom-pkgs/git_upload.nix
+++ custom-pkgs/git_upload.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  git_up = pkgs.writeText "git_up" ''
+
+  gitup = pkgs.writeScriptBin "gitup" ''
         #!/bin/bash
 
         # Tolga Erok
@@ -26,23 +27,7 @@ let
         git config --global diff.algorithm histogram
         git config --global http.postBuffer 524288000
 
-        if [[ $remote_url == *"git@github.com"* ]]; then
-            echo "Remote URL is set to SSH. Proceeding with the script..."
-        else
-            echo "Remote URL is not set to SSH. Please set up SSH key-based authentication for the remote repository."
-            echo "If you haven't already, generate an SSH key pair:"
-            echo "ssh-keygen -t ed25519 -C 'your email'"
-            echo "Add your SSH key to the agent:"
-            echo "eval $(ssh-agent -s)"
-            echo "ssh-add ~/.ssh/id_ed25519"
-            echo "Then, add your SSH public key to your GitHub account:"
-            echo "cat ~/.ssh/id_ed25519.pub"
-            echo "Finally, update your Git configuration to use SSH:"
-            echo "git config --global credential.helper store"
-            echo "Remote URL needs to be updated to SSH. Exiting..."
-            exit 1
-        fi
-
+        
         git_dir="$HOME/nixos/nixos.git"
         work_tree="$HOME/nixos"
 
@@ -81,6 +66,9 @@ let
 
 in {
 
-  environment.systemPackages = [ (pkgs.writeScriptBin "git_up" git_up) ];
+  #---------------------------------------------------------------------
+  # Type: gitup in terminal to execute above bash script
+  #---------------------------------------------------------------------
 
+  environment.systemPackages = [ gitup ];
 }
