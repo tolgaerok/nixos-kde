d49baa910ef8e5fc89bc234d14073c55f76283c6
tolgaerok <kingtolga@gmail.com>
2023-08-20 23:42:29 +0800

    Update at 11:42 PM

--- custom-pkgs/default.nix
+++ custom-pkgs/default.nix
@@ -3,14 +3,14 @@
   imports = [
 
     #---------------------------------------------------------------------
-    # Import custom in-house environment packages
+    # Import custom in-house environment packages:
     #---------------------------------------------------------------------
 
+    # ./git_upload.nix
     # ./rsync-home-to-server.nix    
     # ./trimmgenerations.nix
     ./copy-back-up.nix
     ./create-smb-user.nix
-    # ./git_upload.nix
     ./make-executable.nix
     ./mounter.nix
     ./my-nix-commands.nix
