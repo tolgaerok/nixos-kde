2168e192a7b17eb12ffc79133477e0b5631a3b21
tolgaerok <kingtolga@gmail.com>
2023-08-26 14:53:16 +0800

    (ツ)_/¯    Update @ 02:53 PM

--- system/firewall/default.nix
+++ system/firewall/default.nix
@@ -3,12 +3,25 @@
   networking = {
 
     enableIPv6 = true;
+
+    # What to display on other platform's network browsers
     hostName = "Tolga-NixOS";
     networkmanager.enable = true;
 
+    # Configure firewall to your likings:
     firewall = {
       allowPing = true;
+
+      # I want to configure my own ports
       enable = false;
+
+      #--------------------------------------------------------------------- 
+      # Curiously, `services.samba` does not automatically open
+      # the needed ports in the firewall. Manualy open ports = [ 139 445 ]
+      #--------------------------------------------------------------------- 
+
+      allowedTCPPorts = [ 22 80 139 443 445 22000 ];
+      allowedUDPPorts = [ 137 138 22000 21027 ];
 
       #--------------------------------------------------------------------- 
       # Adding a rule to the iptables firewall to allow NetBIOS name 
@@ -17,14 +30,6 @@
 
       extraCommands =
         "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns";
-
-      #--------------------------------------------------------------------- 
-      # Curiously, `services.samba` does not automatically open
-      # the needed ports in the firewall.
-      #--------------------------------------------------------------------- 
-
-      allowedTCPPorts = [ 22 80 139 443 445 22000 ];
-      allowedUDPPorts = [ 137 138 22000 21027 ];
 
     };
   };
