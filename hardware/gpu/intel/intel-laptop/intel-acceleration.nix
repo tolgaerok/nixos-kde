958760493780e9579e1a3c63155470e29d811e65
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-08-27 09:48:53 +0800

    Update intel-acceleration.nix

--- hardware/gpu/intel/intel-laptop/intel-acceleration.nix
+++ hardware/gpu/intel/intel-laptop/intel-acceleration.nix
@@ -1,37 +1,33 @@
 { config, pkgs, ... }:
 
-# Laptop { various}
-# Intel hardware video acceleration and
-# VAAPI/VDPAU compatibility, which are more
-# relevant for Intel and AMD GPU's
-
 {
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  # Laptop { various} 
+  # Intel hardware video acceleration and 
+  # VAAPI/VDPAU compatibility, which are more 
+  # relevant for Intel and AMD GPU's 
+
+  nixpkgs.config.packageOverrides = pkgs: { 
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; }; 
   };
 
-  
-# ---------------------------------------------------------------------
-  # Enable Intel GPU in NixOS
+  # --------------------------------------------------------------------- 
+  # Enable Intel GPU in NixOS 
   # ---------------------------------------------------------------------
-  services.xserver = {
-    videoDrivers = [ "intel" ]; # Enable Intel graphics driver
+  services.xserver = { 
+    videoDrivers = [ "intel" ]; # Enable Intel graphics driver 
   };
 
-
-  # Power management
-  # ---------------------------------------------------------------------
+  # Power management 
+  # --------------------------------------------------------------------- 
   powerManagement.enable = true;
 
-  # ---------------------------------------------------------------------
-  # CPU performance scaling
+  # CPU performance scaling 
   # ---------------------------------------------------------------------
   services.thermald.enable = true;
 
+  # Enable tlp 
   # ---------------------------------------------------------------------
-  # Enable tlp
-  # ---------------------------------------------------------------------
-  services.tlp = {
+  services.tlp = { 
     enable = true;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
@@ -45,30 +41,28 @@
       CPU_MIN_PERF_ON_BAT = 0;
       CPU_MAX_PERF_ON_BAT = 20;
 
-      # This enables tlp and sets the minimum and maximum frequencies
-      # for the cpu based on whether it is plugged into power or not. It also 
-      # changes the cpu scaling governor based on this.
+      # This enables tlp and sets the minimum and maximum frequencies 
+      # for the CPU based on whether it is plugged into power or not. It also  
+      # changes the CPU scaling governor based on this.
 
     };
   };
 
+  # Use this instead if laptop runs HOT under tlp 
+  # Tell tlp to always run in battery mode 
   # ---------------------------------------------------------------------
-  # Use this instead if laptop runs HOT under tlp
-  # Tell tlp to always run in battery mode
-  # ---------------------------------------------------------------------
-
-  #services.tlp = {
-  #  enable = true;
-  #  settings = {
-  #    TLP_DEFAULT_MODE = "BAT";
-  #    TLP_PERSISTENT_DEFAULT = 1;
-  #  };
-  #};
-
-  # ---------------------------------------------------------------------
-  # Configure auto-cpufreq
-  # Comment all this out if theres a conflict with tlp or use one or the 
-  # other (auto cpu or tlp)
+  
+  #services.tlp = { 
+  #  enable = true; 
+  #  settings = { 
+  #    TLP_DEFAULT_MODE = "BAT"; 
+  #    TLP_PERSISTENT_DEFAULT = 1; 
+  #  }; 
+  #}; 
+  
+  # Configure auto-cpufreq 
+  # Comment all this out if there's a conflict with tlp or use one or the  
+  # other (auto CPU or tlp) 
   # ---------------------------------------------------------------------
   services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
@@ -81,53 +75,50 @@
       turbo = "auto";
     };
 
-    # power management is auto-cpufreq which aims to replace tlp. 
-    # When using auto-cpufreq it is therefore recommended to disable tlp as 
-    # these tools are conflicting with each other. However, NixOS does allow 
+    # power management is auto-cpufreq which aims to replace tlp.  
+    # When using auto-cpufreq it is therefore recommended to disable tlp as  
+    # these tools are conflicting with each other. However, NixOS does allow  
     # for using both at the same time, and you therefore run them in tandem at your own risk.
 
   };
 
-  # ---------------------------------------------------------------------
-  # Hardware video acceleration and compatibility for Intel GPUs
+  # Hardware video acceleration and compatibility for Intel GPUs 
   # ---------------------------------------------------------------------
   hardware.opengl = {
     enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
+    extraPackages = with pkgs; [ 
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD 
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium) 
+      vaapiVdpau 
+      libvdpau-va-gl 
     ];
   };
 
+  # Use this snippet if you have Hybrid graphics. Many laptops have both  
+  # a dedicated and a discrete GPU {Nvidia & Intel} 
   # ---------------------------------------------------------------------
-  # Use this snippet if you have Hybrid graphics. Many laptops have both 
-  # a dedicated and a discrete GPU {Nvidia & Intel}
-  # ---------------------------------------------------------------------
+  
+  #specialisation = { 
+  #nvidia.configuration = { 
+  # Nvidia Configuration 
+  # services.xserver.videoDrivers = [ "nvidia" ]; 
+  # hardware.opengl.enable = true; 
 
-  #specialisation = {
-  #nvidia.configuration = {
-  # Nvidia Configuration
-  # services.xserver.videoDrivers = [ "nvidia" ];
-  # hardware.opengl.enable = true;
+  # Optionally, you may need to select the appropriate driver version for your specific GPU. 
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable; 
 
-  # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # nvidia-drm.modeset=1 is required for some Wayland compositors, e.g. sway 
+  # hardware.nvidia.modesetting.enable = true; 
 
-  # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
-  # hardware.nvidia.modesetting.enable = true;
+  # hardware.nvidia.prime = { 
+  #   sync.enable = true; 
 
-  # hardware.nvidia.prime = {
-  #   sync.enable = true;
+  #   # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA 
+  #   nvidiaBusId = "PCI:1:0:0"; 
 
-  #   # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-  #   nvidiaBusId = "PCI:1:0:0";
-
-  #   # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-  #   intelBusId = "PCI:0:2:0";
+  #   # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA 
+  #   intelBusId = "PCI:0:2:0"; 
   # };
   #};
   #};
-
 }
