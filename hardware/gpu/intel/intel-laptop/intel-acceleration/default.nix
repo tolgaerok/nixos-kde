b526a390f0dc2bf6e1775f8d14536b3f6768ab55
tolgaerok <kingtolga@gmail.com>
2023-08-28 00:19:32 +0800

    (ツ)_/¯    Update @ 12:19 AM

--- hardware/gpu/intel/intel-laptop/intel-acceleration/default.nix
+++ hardware/gpu/intel/intel-laptop/intel-acceleration/default.nix
@@ -33,11 +33,17 @@
   # ---------------------------------------------------------------------
   hardware.opengl = {
     enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+
+      amdvlk
+      intel-media-driver  # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel          # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       vaapiVdpau
       libvdpau-va-gl
+
     ];
   };
 
