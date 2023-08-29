d07b4847c61b30a5e7592e2eeaa44b2aa3f45dc7
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-08-29 13:06:37 +0800

    Update nvidia-stable.nix

--- hardware/gpu/nvidia/nvidia-stable/nvidia-stable.nix
+++ hardware/gpu/nvidia/nvidia-stable/nvidia-stable.nix
@@ -12,16 +12,21 @@
   hardware.nvidia.modesetting.enable = true;
   hardware.opengl.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;   # Works well with Nvidia GT-1030 onwards
 
+  # -----------------------------------------------------------------------------
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  # -----------------------------------------------------------------------------
 
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
 
+  # -----------------------------------------------------------------------------
   # Check legacy drivers https://www.nvidia.com/en-us/drivers/unix/legacy-gpu/
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_340
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_390
+  # -----------------------------------------------------------------------------
+
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_390;  # Works well with Nvidia GT-710 and below
 
 }
