3c64b6e9239a2ccc96079347c0646db122866215
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-08-29 11:27:24 +0800

    Update RUN-ME-AFTER -1ST-REBUILD-NO-SUDO.sh
    code errors

--- RUN-ME-AFTER -1ST-REBUILD-NO-SUDO.sh
+++ RUN-ME-AFTER -1ST-REBUILD-NO-SUDO.sh
@@ -3,24 +3,32 @@
 start_time=$(date +%s)
 
 # Paths
+
 font_folder="/etc/nixos/SETUP/fonts"
-font_dest="/home/$(whoami)"
+font_dest="/home/$(whoami)/.local/share/fonts"
+
 script_folder="/etc/nixos/SETUP/scripts"
 script_dest="/home/$(whoami)"
+
 wallpapers_src="/etc/nixos/SETUP/wallpapers"
 wallpapers_dest="/home/$(whoami)/Pictures"
 
 # Create destination folders if they don't exist
+mkdir -p "$font_dest"
 mkdir -p "$script_dest"
 mkdir -p "$wallpapers_dest"
 
+# Move the fonts folder and its contents
+cp -r "$font_folder" "$font_dest"
+notify-send --app-name="DONE" "Fonts folder copied into $(whoami)" "$font_dest" -u normal
+
 # Move the scripts folder and its contents
 cp -r "$script_folder" "$script_dest"
-notify-send --app-name="DONE" "Script folder copied into $(whoami)" "/home directory" -u normal
+notify-send --app-name="DONE" "Script folder copied into $(whoami)" "$script_dest" -u normal
 
 # Move the wallpapers folder and its contents
 cp -r "$wallpapers_src" "$wallpapers_dest"
-notify-send --app-name="DONE" "Wallpaper folder moved into $(whoami)" "/Pictures directory" -u normal
+notify-send --app-name="DONE" "Wallpaper folder moved into $(whoami)" "$wallpapers_dest" -u normal
 
 # Set permissions for the destination folders and their contents
 chmod -R +x "$script_dest"
@@ -31,10 +39,11 @@ user_name=$(whoami)
 user_group=$(id -gn "$user_name")
 
 # Set ownership to the user and user's group
+chown -R "$user_name":"$user_group" "$font_dest"
 chown -R "$user_name":"$user_group" "$script_dest"
 chown -R "$user_name":"$user_group" "$wallpapers_dest"
 
-echo "Scripts and wallpapers folders have been moved to your home directory and permissions set."
+echo "Scripts, fonts and wallpapers folders have been moved to your home directory and permissions set."
 
 end_time=$(date +%s)
 time_taken=$((end_time - start_time))
