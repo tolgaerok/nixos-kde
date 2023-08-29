982156898a1e8bb8d048f20df72c6a40e9fe6a42
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-08-29 12:55:51 +0800

    Update default.nix
    merge issues

--- packages/andriod/default.nix
+++ packages/andriod/default.nix
@@ -6,21 +6,11 @@
       # Andriod Utilities     
       # adbfs-rootless
       # haskellPackages.adb
-      android-tools
-<<<<<<< HEAD
-      android-file-transfer
-      android-tools
-      droidcam
-      scrcpy
-      waydroid
-
-=======
+      android-tools      
       android-file-transfer   # added by wvpianoman
       droidcam                # added by wvpianoman
       scrcpy                  # added by wvpianoman
       waydroid                # added by wvpianoman
-      
->>>>>>> 961fa1279141f9f772604096a57956ac994c4c27
     ];
   };
 }
