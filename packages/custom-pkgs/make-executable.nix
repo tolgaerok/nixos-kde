a3e7b5d1347561a46491ddbf4a2d988dc3518122
tolgaerok <kingtolga@gmail.com>
2023-08-29 14:01:43 +0800

    (ツ)_/¯    Update @ 02:01 PM

--- packages/custom-pkgs/make-executable.nix
+++ packages/custom-pkgs/make-executable.nix
@@ -17,6 +17,7 @@ folder="$(pwd)"
 # Find all files ending with the .sh extension inside the folder
 # find "$folder" -type f -name "*.sh" -exec chmod u+x {} \; -exec bash -c 'echo -e "\e[34mFolder: \e[97m'"$folder"'\e[0m\n\e[33mScript: \e[94m{} \e[0m\n\e[32mNow executable.\e[0m"' \;
 find "$folder" -type f \( -name "*.sh" -o -name "*.nix" \) -exec chmod u+x {} \; -exec bash -c 'echo -e "\e[34mFolder: \e[97m'"$folder"'\e[0m\n\e[33mScript: \e[94m{} \e[0m\n\e[32mNow executable.\e[0m"' \;
+exit 1
   '';
 
 in {
