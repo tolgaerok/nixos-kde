b5c71679a511ce3bc0e7ca26ec3f8f4c0e585f4e
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-08-29 11:58:40 +0800

    Update RUN-ME-AFTER -1ST-REBUILD-NO-SUDO.sh
    Code tidy up

--- RUN-ME-AFTER -1ST-REBUILD-NO-SUDO.sh
+++ RUN-ME-AFTER -1ST-REBUILD-NO-SUDO.sh
@@ -2,8 +2,15 @@
 #!/bin/bash
 start_time=$(date +%s)
 
-# Paths
+# Install notify agents
+nix-env -iA nixos.libnotify
+nix-env -iA nixos.notify-desktop
 
+# Install Samba
+nix-env -iA nixos.cifs-utils
+nix-env -iA nixos.samba4Full
+
+# Paths
 font_folder="/etc/nixos/SETUP/fonts"
 font_dest="/home/$(whoami)/.local/share/fonts"
 
@@ -20,17 +27,18 @@ mkdir -p "$wallpapers_dest"
 
 # Move the fonts folder and its contents
 cp -r "$font_folder" "$font_dest"
-notify-send --app-name="DONE" "Fonts folder copied into $(whoami)" "$font_dest" -u normal
+notify-send --icon=font-collection --app-name="DONE" "Fonts folder copied into $(whoami)" "$font_dest" -u normal
 
 # Move the scripts folder and its contents
 cp -r "$script_folder" "$script_dest"
-notify-send --app-name="DONE" "Script folder copied into $(whoami)" "$script_dest" -u normal
+notify-send --icon=text-x-script --app-name="DONE" "Script folder copied into $(whoami)" "$script_dest" -u normal
 
 # Move the wallpapers folder and its contents
 cp -r "$wallpapers_src" "$wallpapers_dest"
-notify-send --app-name="DONE" "Wallpaper folder moved into $(whoami)" "$wallpapers_dest" -u normal
+notify-send --icon=litedesktop --app-name="DONE" "Wallpaper folder moved into $(whoami)" "$wallpapers_dest" -u normal
 
 # Set permissions for the destination folders and their contents
+chmod -R +x "$font_dest"
 chmod -R +x "$script_dest"
 chmod -R +w "$wallpapers_dest"
 
@@ -48,7 +56,7 @@ echo "Scripts, fonts and wallpapers folders have been moved to your home directo
 end_time=$(date +%s)
 time_taken=$((end_time - start_time))
 
-notify-send --app-name="Script Timer" "Script Execution Complete" "Time taken: $time_taken seconds" -u normal
+notify-send --icon=ktimetracker --app-name="Post set-up" "Basic set-up Complete" "Time taken: $time_taken seconds" -u normal
 
 # Change directory to the SETUP directory
 cd SETUP
