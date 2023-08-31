7764ee791aa6a7a5bd114b8fa18b9e59956b8516
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-08-31 19:19:59 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,4 +2,25 @@
 
 # Tolga
 
-echo "Rsync completed from "
+# Prompt user to select source location using file manager
+source_location=$(xdg-open "$(zenity --file-selection --title="Select Source Location")")
+
+# Check if user canceled the source selection
+if [ $? -ne 0 ]; then
+    echo "Source selection canceled."
+    exit 1
+fi
+
+# Prompt user to select destination location using file manager
+destination_location=$(xdg-open "$(zenity --file-selection --title="Select Destination Location")")
+
+# Check if user canceled the destination selection
+if [ $? -ne 0 ]; then
+    echo "Destination selection canceled."
+    exit 1
+fi
+
+# Perform the rsync
+rsync -av "$source_location" "$destination_location"
+
+echo "Rsync completed from '$source_location' to '$destination_location'."
