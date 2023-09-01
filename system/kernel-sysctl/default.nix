7ca44ec0478869b4f26c822c0b3f76a6152ff569
tolgaerok <kingtolga@gmail.com>
2023-09-02 07:56:23 +0800

    (ツ)_/¯    Update @ 07:56 AM

--- system/kernel-sysctl/default.nix
+++ system/kernel-sysctl/default.nix
@@ -19,14 +19,57 @@
     "vm.dirty_background_bytes" = 16777216;   # 16 MB data that has been modified in memory and needs to be written to disk
     "vm.dirty_bytes" = 50331648;              # 48 MB data that has been modified in memory and needs to be written to disk
     "vm.min_free_kbytes" = 65536;             # Minimum free memory for safety (in KB), can help prevent memory exhaustion situations
-    "vm.swappiness" = 1;                      # how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM,
+    "vm.swappiness" = 30;                     # how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM,
     "vm.vfs_cache_pressure" = 50;             # Adjust vfs_cache_pressure (0-1000), how the kernel reclaims memory used for caching filesystem objects
   };
 
 }
+
+# -----------------------------------------------------------------
+# Summary of my configuration
+# -----------------------------------------------------------------
+
+# Network Performance Settings:
+# ----------------------------------------------
+# "net.core.rmem_default" = 16 MB
+# "net.core.rmem_max" = 16 MB
+# "net.core.wmem_default" = 16 MB
+# "net.core.wmem_max" = 16 MB
+# These settings define the default and maximum socket buffer sizes for receiving and sending data, improving network performance and benefiting applications that use sockets.
+
+# TCP Keepalive Settings:
+# ----------------------------------------------
+# "net.ipv4.tcp_keepalive_intvl" = 30 seconds
+# "net.ipv4.tcp_keepalive_probes" = 5 probes
+# "net.ipv4.tcp_keepalive_time" = 300 seconds
+# These settings configure TCP keepalive parameters, which are used to detect if a network connection is still alive.
+
+# Disk Write Behavior Settings:
+# ----------------------------------------------
+# "vm.dirty_background_bytes" = 16 MB
+# "vm.dirty_bytes" = 48 MB
+# These settings control how much modified data in memory needs to be written to disk. Lower values can lead to more frequent writes.
+
+# Memory Safety Setting:
+# ----------------------------------------------
+# "vm.min_free_kbytes" = 65,536 KB
+# This setting specifies the minimum amount of free memory in kilobytes, helping to prevent memory exhaustion situations.
+
+# Swappiness Setting:
+# ----------------------------------------------
+# "vm.swappiness" = 1
+# This setting determines how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM, suitable for systems with ample memory.
+
+# Cache Pressure Setting:
+# ----------------------------------------------
+# "vm.vfs_cache_pressure" = 50
+# This setting adjusts vfs_cache_pressure (0-1000), influencing how the kernel reclaims memory used for caching filesystem objects.
+
+# Customize these settings based on your system's requirements and performance goals, check below for extra info.
+
 # Values explained:
 # "vm.vfs_cache_pressure" = 50; Adjust vfs_cache_pressure (0-1000)
-
+# -----------------------------------------------------------------
 # 4GB RAM:  50 - 60 [ Lower values like 50 or 60 can be a good starting point for systems with 4GB of RAM to retain data in cache for improved file system access times ]
 # 8GB RAM:  60 - 80 [ Values around 60 to 80 are reasonable for 8GB of RAM, as you can still afford to keep more data in cache for better performance ]
 # 16GB RAM: 70 - 80 [ Lower values like 70 or 80 can be used to keep data in cache longer for systems with 16GB of RAM ]
@@ -43,7 +86,18 @@
 # Servers or virtual machines where memory availability is critical.
 # When you want to ensure that cached memory is released more aggressively for other applications.
 
+# vm.swappiness = <Recommended Value>
+# ----------------------------------------------
+# Adjusts how aggressively the kernel swaps data from RAM to disk.
+# - For systems with 4GB RAM, consider a low value (1-10) to prioritize keeping data in RAM.
+# - For systems with 8GB RAM, use a moderate value (10-30) for a balance between RAM usage and swapping.
+# - For systems with 16GB to 28GB RAM, aim for a moderate value (10-30) to optimize performance.
+# - For systems with 32GB or more RAM, a slightly higher value (30-60) may be suitable.
+# Monitor system performance and adjust as needed based on your workload and available RAM.
+
+
 # CALCULATIONS: ( Schooling )
+# ----------------------------------------------
 # 1 MB (megabyte) = 1048576 bytes
 # To convert bytes to megabytes, divide the number of bytes by 1048576.
 
