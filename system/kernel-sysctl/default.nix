af0132042d45c26ab6fd1323e66e3cfce796568f
tolgaerok <kingtolga@gmail.com>
2023-09-02 07:35:47 +0800

    (ツ)_/¯    Update @ 07:35 AM

--- system/kernel-sysctl/default.nix
+++ system/kernel-sysctl/default.nix
@@ -16,8 +16,8 @@
     "net.ipv4.tcp_keepalive_intvl" = 30;      # TCP keepalive interval between probes, TCP keepalive probes, which are used to detect if a connection is still alive.
     "net.ipv4.tcp_keepalive_probes" = 5;      # TCP keepalive probes, TCP keepalive probes, which are used to detect if a connection is still alive.
     "net.ipv4.tcp_keepalive_time" = 300;      # TCP keepalive interval (seconds), TCP keepalive probes, which are used to detect if a connection is still alive.
-    "vm.dirty_background_bytes" = 268435456;  # 256 MB in bytes, data that has been modified in memory and needs to be written to disk
-    "vm.dirty_bytes" = 1073741824;            # 1 GB in bytes, data that has been modified in memory and needs to be written to disk
+    "vm.dirty_background_bytes" = 16777216;   # 16 MB data that has been modified in memory and needs to be written to disk
+    "vm.dirty_bytes" = 50331648;              # 48 MB data that has been modified in memory and needs to be written to disk
     "vm.min_free_kbytes" = 65536;             # Minimum free memory for safety (in KB), can help prevent memory exhaustion situations
     "vm.swappiness" = 1;                      # how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM,
     "vm.vfs_cache_pressure" = 50;             # Adjust vfs_cache_pressure (0-1000), how the kernel reclaims memory used for caching filesystem objects
