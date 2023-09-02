0f3f9d8678fa64baf70da1e6bd52f5ca7051b4ab
tolgaerok <kingtolga@gmail.com>
2023-09-02 21:50:38 +0800

    Added: themechanger

--- packages/misc/default.nix
+++ packages/misc/default.nix
@@ -1,17 +1,19 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   # Miscellaneous:
   environment = {
     systemPackages = with pkgs; [
 
-      cowsay                  # fortune | cowsay -f tux   fortune | cowsay -e ^^    fortune | cowsay | lolcat
+      cowsay # fortune | cowsay -f tux   fortune | cowsay -e ^^    fortune | cowsay | lolcat
       fish
       flatpak
-      fortune                 # unstr rot strfile fortune
+      fortune # unstr rot strfile fortune
       libsForQt5.kweather
       libsForQt5.kweathercore
-      lolcat                  # "lolcat" for colorful output
+      lolcat # "lolcat" for colorful output
       variety
+
+      papirus-icon-theme
+      themechanger
 
     ];
   };
