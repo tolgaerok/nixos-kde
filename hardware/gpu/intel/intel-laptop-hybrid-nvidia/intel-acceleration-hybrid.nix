d7e8fb1d8ac0a680c481f5c8ebb51b36a469ed16
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-09-02 08:16:59 +0800

    (ツ)_/¯ Update @ 8:17 AM - Removed duplicate enteries

--- hardware/gpu/intel/intel-laptop-hybrid-nvidia/intel-acceleration-hybrid.nix
+++ hardware/gpu/intel/intel-laptop-hybrid-nvidia/intel-acceleration-hybrid.nix
@@ -76,6 +76,7 @@
   # Comment all this out if theres a conflict with tlp or use one or the 
   # other (auto cpu or tlp)
   # ---------------------------------------------------------------------
+
   services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
     battery = {
@@ -111,21 +112,7 @@
   };
 
   # ---------------------------------------------------------------------
-  # Use this snippet if you have Hybrid graphics. Many laptops have both 
-  # a dedicated and a discrete GPU {Nvidia & Intel}
-  # ---------------------------------------------------------------------
-
-  #specialisation = {
-  #nvidia.configuration = {
-  # Nvidia Configuration
-  # services.xserver.videoDrivers = [ "nvidia" ];
-  # hardware.opengl.enable = true;
-
-  # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-  # nvidia-drm.modeset=1 is required for some wayland # ---------------------------------------------------------------------
-  # Use this snippet if you have Hybrid graphics. Many laptops have both 
+  # Use this below snippet only if you have Hybrid graphics. Many laptops have both 
   # a dedicated and a discrete GPU {Nvidia & Intel}
   # ---------------------------------------------------------------------
 
@@ -139,19 +126,6 @@
   # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 
   # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
-  # hardware.nvidia.modesetting.enable = true;
-
-  # hardware.nvidia.prime = {
-  #   sync.enable = true;
-
-  #   # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-  #   nvidiaBusId = "PCI:1:0:0";
-
-  #   # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-  #   intelBusId = "PCI:0:2:0";
-  # };
-  #};
-  #};compositors, e.g. sway
   # hardware.nvidia.modesetting.enable = true;
 
   # hardware.nvidia.prime = {
