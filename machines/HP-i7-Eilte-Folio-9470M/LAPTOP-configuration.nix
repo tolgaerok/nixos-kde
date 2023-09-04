5bbdd984834b68f4ed0b0fa574014becbe40ff8e
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-09-04 23:05:04 +0800

    Update LAPTOP-configuration.nix

--- machines/HP-i7-Eilte-Folio-9470M/LAPTOP-configuration.nix
+++ machines/HP-i7-Eilte-Folio-9470M/LAPTOP-configuration.nix
@@ -7,9 +7,9 @@
 {
   imports = [
 
-    # ./hardware/gpu/nvidia/nvidia-opengl/nvidia-opengl.nix  # NVIDIA with hardware acceleration (Open-GL) for GT-1030++
-    # ./hardware/gpu/nvidia/nvidia-stable/nvidia-stable.nix  # NVIDIA stable for GT-710--
-    ./hardware/gpu/intel/intel-laptop # INTEL GPU with (Open-GL), tlp and auto-cpufreq
+    # ./hardware/gpu/nvidia/nvidia-opengl/nvidia-opengl.nix    # NVIDIA with hardware acceleration (Open-GL) for GT-1030++
+    # ./hardware/gpu/nvidia/nvidia-stable/nvidia-stable.nix    # NVIDIA stable for GT-710--
+    ./hardware/gpu/intel/intel-laptop                          # INTEL GPU with (Open-GL), tlp and auto-cpufreq
     ./hardware-configuration.nix
     ./nix
     ./packages
