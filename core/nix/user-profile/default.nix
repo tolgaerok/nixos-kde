7b347a712f6005e38eb9e33a53088bf90c9b21ee
tolgaerok <kingtolga@gmail.com>
2023-09-09 18:08:17 +0800

    (ツ)_/¯    Update @ 06:08 PM

--- core/nix/user-profile/default.nix
+++ core/nix/user-profile/default.nix
@@ -8,7 +8,7 @@
   system.activationScripts.setGnomeProfilePicture = ''
     mkdir -p /var/lib/AccountsService/icons
     if [[ ! -h /var/lib/AccountsService/icons/tolga ]]; then
-      cp /etc/nixos/SETUP/profile-pics/cool-tolga.png /var/lib/AccountsService/icons/tolga
+      cp /etc/nixos/SETUP/profile-pics/cool-tolga-2.png /var/lib/AccountsService/icons/tolga
     fi
   '';
 }
