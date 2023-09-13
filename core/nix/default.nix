904bac19855ae797e6fc311d1dfa40e939e6157f
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-09-13 22:43:13 +0800

    Add files via upload

---
+++ core/nix/default.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, lib, ... }:
+
+let
+
+  name = "tolga";
+
+in {
+
+  imports = [
+
+    ./dconf
+    ./nixpkgs-config
+    ./user-profile
+  ];
+
+  # Nix-specific settings and garbage collection options - Mostly research from NixOS wiki
+
+  # optimise.automatic = true;
+
+  nix = {
+    settings = {
+      allowed-users = [ "@wheel" ];
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      keep-derivations = true;
+      keep-outputs = true;
+      sandbox = true;
+      trusted-users = [ "root" "${name}" ];
+      warn-dirty = false;
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  # trim deleted blocks from ssd
+  services.fstrim.enable = true;
+
+  services.sshd.enable = true;
+
+  # services.fwupd.enable = true;
+}
