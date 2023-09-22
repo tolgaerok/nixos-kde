8ad4201342e6b97c2e03dcaf0ddfebd5c7317075
Tolga Erok <kingtolga@gmail.com>
2023-09-22 11:27:11 +0800

    (ツ)_/¯    Update @ 11:27 AM

--- core/packages/packages.nix
+++ core/packages/packages.nix
@@ -15,6 +15,9 @@
 
       android-file-transfer           # aft-mtp-cli android-file-transfer aft-mtp-mount 
       android-tools                   # lpadd append2simg lpmake mke2fs.android mkdtboimg simg2img lpdump lpunpack ext2simg e2fsdroid adb unpack_bootimg repack_bootimg avbtool img2simg fastboot mkbootimg lpflash
+      droidcam                        # Linux client for DroidCam app 
+      scrcpy                          # Display and control Android devices over USB or TCP/IP
+      waydroid                        # Waydroid is a container-based approach to boot a full Android system on a regular GNU/Linux system
 
       # ---------------------------------------------------------------------
       # Archive Utilities
@@ -24,10 +27,10 @@
       gzip                            # gunzip zmore zegrep zfgrep zdiff zcmp uncompress gzip znew zless zcat zforce gzexe zgrep
       lz4                             # lz4c lz4 unlz4 lz4cat
       lzip                            # lzip
-      lzo
+      lzo                             # Real-time data (de)compression library
       lzop                            # lzop
       p7zip                           # 7zr 7z 7za
-      rar
+      rar                             # Utility for RAR archives
       rzip                            # rzip
       unzip                           # zipinfo unzipsfx zipgrep funzip unzip
       xz                              # lzfgrep lzgrep lzma xzegrep xz unlzma lzegrep lzmainfo lzcat xzcat xzfgrep xzdiff lzmore xzgrep xzdec lzdiff xzcmp lzmadec xzless xzmore unxz lzless lzcmp
@@ -41,8 +44,8 @@
       audacity                        # audacity
       ffmpeg                          # ffprobe ffmpeg
       ffmpegthumbnailer               # ffmpegthumbnailer
-      libdvdcss
-      libdvdread
+      libdvdcss                       # A library for decrypting DVDs
+      libdvdread                      # A library for reading DVDs
       libopus
       libvorbis
       mediainfo                       # mediainfo
@@ -124,37 +127,38 @@
       # Dsctool
       # ---------------------------------------------------------------------
 
-      dvc
-      gnuplot
-      iredis
-      litecli
-      luigi
-      mpi
-      quarto
-      root
-      visidata
+      dvc                            # Version Control System for Machine Learning Projects
+      gnuplot                        # A portable command-line driven graphing utility for many platforms
+      iredis                         # A Terminal Client for Redis with AutoCompletion and Syntax Highlighting
+      litecli                        # Command-line interface for SQLite
+      luigi                          # Python package that helps you build complex pipelines of batch jobs
+      mpi                            # Open source MPI-3 implementation
+      quarto                         # Open-source scientific and technical publishing system built on Pandoc
+      root                           # A data analysis framework
+      visidata                       # Interactive terminal multitool for tabular data
 
       # ---------------------------------------------------------------------
       # Scanning and Image Viewing
       # ---------------------------------------------------------------------
 
-      nsxiv
-      sane-backends
-      scanbd
-      sxiv
+      nsxiv                          # New Suckless X Image Viewe
+      sane-backends                  # SANE (Scanner Access Now Easy) backends
+      scanbd                         # Scanner button daemon
+      sxiv                           # Simple X Image Viewer
 
       # ---------------------------------------------------------------------
       # Downloading Videos and Files
       # ---------------------------------------------------------------------
 
-      clipgrab
-      wget
+      clipgrab                       # Video downloader for YouTube and other sites
+      wget                           # Tool for retrieving files using HTTP, HTTPS, and FTP
 
       # ---------------------------------------------------------------------
       # Messaging and Communication:
       # ---------------------------------------------------------------------
 
-      whatsapp-for-linux
+      whatsapp-for-linux             # Whatsapp desktop messaging app
+
 
       # ---------------------------------------------------------------------
       # Scientific computing
@@ -166,73 +170,85 @@
       # Miscellaneous:
       # ---------------------------------------------------------------------
 
-      cowsay                      # fortune | cowsay -f tux   fortune | cowsay -e ^^    fortune | cowsay | lolcat
-      fish
-      flatpak
-      fortune                     # unstr rot strfile fortune
+      cowsay                         # A program which generates ASCII pictures of a cow with a message
+                                     #
+                                     # ex: $ fortune | cowsay -f tux  
+                                     #     $ fortune | cowsay -e ^^   
+                                     #     $ fortune | cowsay | lolcat
+      fish                           # Smart and user-friendly command line shell
+      flatpak                        # Linux application sandboxing and distribution framework
+      fortune                        # unstr rot strfile fortune
       libsForQt5.kweather
       libsForQt5.kweathercore
-      lolcat                      # "lolcat" for colorful output
-      themechanger
-      variety
+      lolcat                         # A rainbow version of cat for colorful output                         
+                                     # "lolcat" for colorful output
+      themechanger                   # A theme changing utility for Linux
+      variety                        # A wallpaper manager for Linux systems
 
       # ---------------------------------------------------------------------
       # Media and Entertainment:
       # ---------------------------------------------------------------------
 
-      vlc
-      youtube-dl
+      vlc                            # Cross-platform media player and streaming server
+      youtube-dl                     # Command-line tool to download videos from YouTube.com and other sites
 
       # ---------------------------------------------------------------------
       # Picture manger
       # ---------------------------------------------------------------------
 
-      digikam
-      shotwell
+      digikam                        # Photo Management Program
+      shotwell                       # Popular photo organizer for the GNOME desktop
 
       # ---------------------------------------------------------------------
       # Picture Editors
       # ---------------------------------------------------------------------
 
-      gimp-with-plugins
+      gimp-with-plugins              # The GNU Image Manipulation Program
 
       # ---------------------------------------------------------------------
       # Disc burner
       # ---------------------------------------------------------------------
 
-      brasero
+      brasero                        # A Gnome CD/DVD Burner
 
       # ---------------------------------------------------------------------
       # Remote Access and Automation:
       # ---------------------------------------------------------------------
 
-      heroku
-      powershell
-      sshpass
-      teamviewer
+      heroku                         # Everything you need to get started using Heroku
+      powershell                     # Powerful cross-platform (Windows, Linux, and macOS) shell and scripting language based on .NET
+      sshpass                        # Non-interactive ssh password auth
+      teamviewer                     # Desktop sharing application, providing remote support and online meetings
 
       # ---------------------------------------------------------------------
       # File Sharing & Network
       # ---------------------------------------------------------------------
 
-      samba4Full
-      cifs-utils
+      samba4Full                     # The standard Windows interoperability suite of programs for Linux and Unix
+      cifs-utils                     # Tools for managing Linux CIFS client filesystems
 
       # ---------------------------------------------------------------------
       # KDE Plasma tools
       # ---------------------------------------------------------------------
 
-      ark
-      filelight
-      kate
-      kcalc
-      kdiff3
-      kgpg
+      kdiff3                         # Compares and merges 2 or 3 files or directories
+      libsForQt5.ark                 # Graphical file compression/decompression utility
+      libsForQt5.filelight           # Disk usage statistics
+      libsForQt5.kate                # Advanced text editor
+      libsForQt5.kcalc               # Scientific calculator
+      libsForQt5.kgpg                # A KDE based interface for GnuPG, a powerful encryption utility
       krename
-      libsForQt5.qt5.qttools        # qhelpgenerator linguist qtplugininfo qdistancefieldgenerator pixeltool qcollectiongenerator assistant qtdiag qdbusviewer lupdate qtpaths qtattributionsscanner lconvert designer lupdate-pro lrelease qdbus lprodump lrelease-pro
-      libsForQt5.quazip             # quazip
-      qt6.qttools                   # assistant qtplugininfo qdbus lrelease linguist qtdiag6 qtdiag qdbusviewer qdistancefieldgenerator pixeltool lconvert lupdate designer
-      qt6Packages.quazip            # quazip
+      libsForQt5.qt5.qttools        # A cross-platform application framework for C++        
+                                    # qhelpgenerator linguist qtplugininfo qdistancefieldgenerator pixeltool qcollectiongenerator assistant qtdiag qdbusviewer lupdate qtpaths qtattributionsscanner lconvert designer lupdate-pro lrelease qdbus lprodump lrelease-pro
+      
+      libsForQt5.quazip             # Provides access to ZIP archives from Qt 5 programs
+                                    # quazip
+
+      qt6.qttools                   # # A cross-platform application framework for C++
+                                    # assistant qtplugininfo qdbus lrelease linguist qtdiag6 qtdiag qdbusviewer qdistancefieldgenerator pixeltool lconvert lupdate designer
+      
+      qt6Packages.quazip            # Provides access to ZIP archives from Qt programs
+                                    # quazip
 
       # ---------------------------------------------------------------------
       # xscreensaver
