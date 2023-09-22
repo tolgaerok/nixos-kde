81beb49b98e831726186e3bac54888d24e8cddba
Tolga Erok <kingtolga@gmail.com>
2023-09-22 12:13:24 +0800

    (ツ)_/¯    Update @ 12:13 PM

--- core/packages/packages.nix
+++ core/packages/packages.nix
@@ -91,8 +91,11 @@
       # Code Search and Analysis:
       # ---------------------------------------------------------------------
 
-      ripgrep                         # rg
-      ripgrep-all                     # rga-preproc rga
+      ripgrep                         # A utility that combines the usability of The Silver Searcher with the raw speed of grep
+                                      # rg
+
+      ripgrep-all                     # Ripgrep, but also search in PDFs, E-Books, Office documents, zip, tar.gz, and more
+                                      # rga-preproc rga
 
       # ---------------------------------------------------------------------
       # Utilities
@@ -224,8 +227,8 @@
       # File Sharing & Network
       # ---------------------------------------------------------------------
 
-      samba4Full                     # The standard Windows interoperability suite of programs for Linux and Unix
       cifs-utils                     # Tools for managing Linux CIFS client filesystems
+      samba4Full                     # The standard Windows interoperability suite of programs for Linux and Unix
 
       # ---------------------------------------------------------------------
       # KDE Plasma tools
@@ -264,22 +267,29 @@
       keepassxc                     # keepassxc keepassxc-cli keepassxc-proxy
       media-downloader              # media-downloader
       testdisk-qt                   # testdisk photorec fidentify qphotorec
-      ventoy-full                   # ventoy   ventoy-persistent   ventoy-web   ventoy-plugson   ventoy-extend-persistent
+      ventoy-full                   #  # A New Bootable USB Solution
+                                    # ventoy   ventoy-persistent   ventoy-web   ventoy-plugson   ventoy-extend-persistent
 
       # ---------------------------------------------------------------------
       # USB and Device Utilities
       # ---------------------------------------------------------------------
 
-      usbutils                      # usb-devices lsusb.py lsusb usbhid-dump
+      usbutils                       # Tools for working with USB devices, such as lsusb
 
       # ---------------------------------------------------------------------
       # Other Miscellaneous Programs
       # ---------------------------------------------------------------------
 
-      blueberry                     # blueberry-tray blueberry
-      efibootmgr                    # efibootdump efibootmgr
+      blueberry                     # Bluetooth configuration tool
+                                    # blueberry-tray blueberry
+
+      efibootmgr                    # A Linux user-space application to modify the Intel Extensible Firmware Interface (EFI) Boot Manager
+                                    # efibootdump efibootmgr
+
       gum                           # gum https://github.com/charmbracelet/gum
-      krusader                      # krusader
+
+      krusader                      # Norton/Total Commander clone for KDE
+                                    # krusader
 
       # ---------------------------------------------------------------------
       # Libraries
@@ -304,62 +314,67 @@
       # Disk Utilities
       # ---------------------------------------------------------------------
 
-      gparted
-      hw-probe                      # sudo -E hw-probe -all -upload
-      ntfs3g
+      gparted                       # Graphical disk partitioning tool
+
+      hw-probe                      # Probe for hardware, check operability and find drivers
+                                    #
+                                    # sudo -E hw-probe -all -upload
+
+      ntfs3g                        # FUSE-based NTFS driver with full write support
 
       # ---------------------------------------------------------------------
       # Terminal Utilities
       # ---------------------------------------------------------------------
 
-      asunder
-      bashInteractive
-      cmatrix
-      cowsay
-      delta
-      direnv
-      duf
-      fd
-      figlet
-      htop
-      imagemagick
-      inotify-tools                 # inotifywait   inotifywatch    https://github.com/inotify-tools/inotify-tools/wiki
-      isoimagewriter
-      less
-      lf
-      lfs
-      lsd
-      lsdvd
-      ncdu
-      neofetch
-      neovim
-      parallel-full
-      pciutils
-      pfetch
-      pkgconfig
-      pmutils
-      psmisc
-      sl
-      stow
-      tig
-      tldr
-      tree
-      vim
-      gnome.zenity                  # For notifications and code dialogs
+      asunder                       # A graphical Audio CD ripper and encoder for Linux
+      bashInteractive               # GNU Bourne-Again Shell, the de facto standard shell on Linux (for interactive use)
+      cmatrix                       # Simulates the falling characters theme from The Matrix movie
+      delta                         # A syntax-highlighting pager for git
+      duf                           # Disk Usage/Free Utility
+      fd                            # A simple, fast and user-friendly alternative to find
+      figlet                        # Program for making large letters out of ordinary text
+      htop                          # An interactive process viewer
+      imagemagick                   # A software suite to create, edit, compose, or convert bitmap images
+
+      inotify-tools                 # A set of command-line programs providing a simple interface to inotify.
+                                    # # inotifywait   inotifywatch
+                                    # Source:  https://github.com/inotify-tools/inotify-tools/wiki
+
+      gnome.zenity                  # Tool to display dialogs from the commandline and shell scripts
+      less                          # A more advanced file pager than ‘more’
+      lf                            # A terminal file manager written in Go and heavily inspired by ranger
+      lfs                           # Get information on your mounted disks
+      lsd                           # The next gen ls command
+      lsdvd                         # Display information about audio, video, and subtitle tracks on a DVD
+      ncdu                          # Disk usage analyzer with an ncurses interface
+      neofetch                      # A fast, highly customizable system info script
+      neovim                        # Vim text editor fork focused on extensibility and agility
+      parallel-full                 #
+      pciutils                      # A collection of programs for inspecting and manipulating configuration of PCI devices
+      pfetch                        # A pretty system information tool written in POSIX sh
+      pmutils                       # A small collection of scripts that handle suspend and resume on behalf of HAL
+      psmisc                        # A set of small useful utilities that use the proc filesystem (such as fuser, killall and pstree)
+      rPackages.pkgconfig           # Set configuration options on a per-package basis. Options set by a given package only apply to that package, other packages are unaffected.
+      sl                            # Steam Locomotive runs across your terminal when you type 'sl'
+      stow                          # A tool for managing the installation of multiple software packages in the same run-time directory tree
+      tig                           # Text-mode interface for git
+      tldr                          # Simplified and community-driven man pages
+      tree                          # Command to produce a depth indented directory listing
+      vim                           # The most popular clone of the VI editor
 
       # ---------------------------------------------------------------------
       # XDG Utilities
       # ---------------------------------------------------------------------
 
-      xdg-launch
-      xdg-utils
+      xdg-launch                    # A command line XDG compliant launcher and tools
+      xdg-utils                     # A set of command line tools that assist applications with a variety of desktop integration tasks
 
       #---------------------------------------------------------------------
       # Office and Productivity:
       #---------------------------------------------------------------------
 
-      wpsoffice
-      deepin.deepin-calculator
+      deepin.deepin-calculator      # An easy to use calculator for ordinary users
+      wpsoffice                     # Office suite, formerly Kingsoft Office
 
     ];
   };
