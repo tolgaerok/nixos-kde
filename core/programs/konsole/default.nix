f737af3375dc44a695598edf2fa05f11ded4dfaa
Tolga Erok <kingtolga@gmail.com>
2023-09-30 00:21:08 +0800

    (ツ)_/¯    Update @ 12:21 AM

--- core/programs/konsole/default.nix
+++ core/programs/konsole/default.nix
@@ -56,8 +56,8 @@
         # Fun stuff
         #---------------------------------------------------------------------
 
-        icons = "sxiv -t /home/tolga/Pictures/icons";
-        wp = "sxiv -t /home/tolga/Pictures/Wallpapers";
+        icons = "sxiv -t $HOME/Pictures/icons";
+        wp = "sxiv -t $HOME/Pictures/Wallpapers";
 
         #---------------------------------------------------------------------
         # File access
