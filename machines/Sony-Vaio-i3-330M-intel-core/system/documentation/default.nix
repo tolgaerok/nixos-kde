9e82a3a4d02505d88a63756956c099e067324b60
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-10-01 17:03:19 +0800

    Update default.nix
    re enable documentations

--- machines/Sony-Vaio-i3-330M-intel-core/system/documentation/default.nix
+++ machines/Sony-Vaio-i3-330M-intel-core/system/documentation/default.nix
@@ -2,10 +2,17 @@
 
 {
 
-  # Disable or remove local documentations
+  # Disable or remove local documentations for faster rebuilding
+  # Only install the docs I use
+
   documentation = {
-    doc.enable = false;
-    info.enable = false;
-    nixos.enable = false;
+
+    doc.enable = true;
+    enable = true; # This enables the overall documentation
+    info.enable = true;
+    man.enable = true; # This enables documentation for 'man'
+    nixos.enable = true;
+    
   };
+
 }
