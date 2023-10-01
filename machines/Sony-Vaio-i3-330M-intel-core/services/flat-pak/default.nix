bd0fea14cedec113ce531e5138482b94d9e59d07
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-10-01 17:12:16 +0800

    Update default.nix
    enable desktop integration for flatpaks

--- machines/Sony-Vaio-i3-330M-intel-core/services/flat-pak/default.nix
+++ machines/Sony-Vaio-i3-330M-intel-core/services/flat-pak/default.nix
@@ -1,10 +1,11 @@
-{ config, pkgs, ... }:
-
 # ---------------------------------------------------------------------
 # Enables support for Flatpak - Flatpak website
 #---------------------------------------------------------------------
 
 {
   services.flatpak.enable = true;
+
+  # For the sandboxed apps to work correctly, desktop integration portals need to be installed. 
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
 }
