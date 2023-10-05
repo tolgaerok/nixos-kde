7c192ce8463f6fd9bacb726a002fc297cade4e19
Tolga Erok <kingtolga@gmail.com>
2023-10-05 18:28:50 +0800

    (ツ)_/¯    Update @ 06:28 PM

--- machines/HP-i5-ProBook-6460b (FULL-CONFIG) /configuration.nix
+++ machines/HP-i5-ProBook-6460b (FULL-CONFIG) /configuration.nix
@@ -101,33 +101,25 @@
 
         # rbs2 =        "sudo nixos-rebuild switch -I nixos-config=$HOME/nixos/configuration.nix";
         garbage = "sudo nix-collect-garbage --delete-older-than 7d";
-        lgens =
-          "sudo nix-env --profile /nix/var/nix/profiles/system --list-generations";
-        neu = "sudo nix-env --upgrade";
-        nopt = "sudo nix-store --optimise";
-        rbs = "sudo nixos-rebuild switch";
-        rebuild-all =
-          "sudo nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot && sudo fstrim -av";
-        switch =
-          "sudo nixos-rebuild switch -I nixos-config=$HOME/nixos/configuration.nix";
+        lgens =   "sudo nix-env --profile /nix/var/nix/profiles/system --list-generations";
+        neu =     "sudo nix-env --upgrade";
+        nopt =    "sudo nix-store --optimise";
+        rbs =     "sudo nixos-rebuild switch";
+        rebuild-all = "sudo nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot && sudo fstrim -av";
+        switch =  "sudo nixos-rebuild switch -I nixos-config=$HOME/nixos/configuration.nix";
 
         #---------------------------------------------------------------------
         # Personal scripts
         #---------------------------------------------------------------------
 
-        htos =
-          "sudo ~/scripts/MYTOOLS/scripts/Zysnc-Options/ZYSNC-HOME-TO-SERVER.sh";
-        master = "sudo ~/scripts/MYTOOLS/main.sh";
-        mount = "sudo ~/scripts/MYTOOLS/scripts/Mounting-Options/MOUNT-ALL.sh";
-        mse = "sudo ~/scripts/MYTOOLS/MAKE-SCRIPTS-EXECUTABLE.sh";
-        mynix =
-          "sudo ~/scripts/MYTOOLS/scripts/COMMAN-NIX-COMMAND-SCRIPT/MyNixOS-commands.sh";
-        stoh =
-          "sudo ~/scripts/MYTOOLS/scripts/Zysnc-Options/ZYSNC-SERVER-TO-HOME.sh";
-        trimgen =
-          "sudo ~/scripts/MYTOOLS/scripts/GENERATION-TRIMMER/TrimmGenerations.sh";
-        umount =
-          "sudo ~/scripts/MYTOOLS/scripts/Mounting-Options/UMOUNT-ALL.sh";
+        htos =    "sudo ~/scripts/MYTOOLS/scripts/Zysnc-Options/ZYSNC-HOME-TO-SERVER.sh";
+        master =  "sudo ~/scripts/MYTOOLS/main.sh";
+        mount =   "sudo ~/scripts/MYTOOLS/scripts/Mounting-Options/MOUNT-ALL.sh";
+        mse =     "sudo ~/scripts/MYTOOLS/MAKE-SCRIPTS-EXECUTABLE.sh";
+        mynix =   "sudo ~/scripts/MYTOOLS/scripts/COMMAN-NIX-COMMAND-SCRIPT/MyNixOS-commands.sh";
+        stoh =    "sudo ~/scripts/MYTOOLS/scripts/Zysnc-Options/ZYSNC-SERVER-TO-HOME.sh";
+        trimgen = "sudo ~/scripts/MYTOOLS/scripts/GENERATION-TRIMMER/TrimmGenerations.sh";
+        umount =  "sudo ~/scripts/MYTOOLS/scripts/Mounting-Options/UMOUNT-ALL.sh";
 
         #---------------------------------------------------------------------
         # Navigate files and directories
