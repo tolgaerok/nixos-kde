5ca4fc4e72a35ac87969952ffeeb2bd615b4a509
Tolga Erok <kingtolga@gmail.com>
2023-10-12 13:33:22 +0800

    (ツ)_/¯    Update @ 01:33 PM

--- machines/VM-NIXOS (FULL-CONFIG)/core/programs/partition-manager/default.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, ...}: {
-  programs = {
-    partition-manager = {
-      enable = true;
-    };
-  };
-}
--- machines/VM-NIXOS (FULL-CONFIG)/user/tolga/sshKeys.nix
+++
@@ -1,5 +0,0 @@
-{
-  tolga = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDrS+VQMWkyNZ70Ym/TZoozhPfLpj9Rx+IlswOK01ZVx kintolga@gmail.com"
-  ];
-}
