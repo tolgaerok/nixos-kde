4be28f3bc63891edc591e20c9ed0f54816e484da
Tolga Erok <kingtolga@gmail.com>
2023-10-13 20:14:48 +0800

    (ツ)_/¯    Update @ 08:14 PM

--- machines/HP-i5-ProBook-6460b (FULL-CONFIG) /core/common/services/xserver/default.nix
+++ machines/HP-i5-ProBook-6460b (FULL-CONFIG) /core/common/services/xserver/default.nix
@@ -20,14 +20,16 @@
       };
     };
 
-    # default settings
+    # ---------------------------------------------------------------------
+    # Vidoe settings that go hand-in-hand with opengl
+    # ---------------------------------------------------------------------
     videoDrivers = [
 
-      "amdgpu"
-      "radeon"
-      "nouveau"
-      "modesetting"
-      "fbdev"
+      "fbdev"             # The fbdev (Framebuffer Device) driver is a generic framebuffer driver that provides access to the frame buffer of the display hardware.
+      # "modesetting"     # The modesetting driver is a generic driver for modern video hardware that relies on kernel modesetting (KMS) to set the display modes and manage resolution and refresh rate.
+      # "amdgpu"          # This is the open-source kernel driver for AMD graphics cards. It's part of the AMDGPU driver stack and provides support for newer AMD GPUs.
+      # "nouveau"         # Nouveau is an open-source driver for NVIDIA graphics cards. It aims to provide support for NVIDIA GPUs and is an alternative to the proprietary NVIDIA driver
+      # "radeon"          # The Radeon driver is an open-source driver for AMD Radeon graphics cards. It provides support for older AMD GPUs.
 
     ];
     
