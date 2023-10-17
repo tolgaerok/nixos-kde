673c7fcfb6317d6f239ff492d9376e4ed330a124
Tolga Erok <kingtolga@gmail.com>
2023-10-17 12:09:23 +0800

    (ツ)_/¯    Update @ 12:09 PM

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, options, lib, ... }:
 
 #---------------------------------------------------------------------
 # Tolga Erok
@@ -8,45 +8,24 @@
 # ¯\_(ツ)_/¯
 #---------------------------------------------------------------------
 
-{
-
-  imports = [
-
-    #---------------------------------------------------------------------
-    # Laptop: Apple MacBook Pro 9.2 i7-3615QM Intel HD Graphics 4000
-    #---------------------------------------------------------------------
-    # ./machines/Apple-MacBookPro-9.2/Apple-configuration.nix
-    
-    #---------------------------------------------------------------------
-    # Laptop: HP-i5-2520M x 4 ProBook 6460b Intel HD Graphics 3000
-    #---------------------------------------------------------------------
-    # ./machines/HP-i5-ProBook-6460b (FULL-CONFIG)/configuration.nix
-
-    #---------------------------------------------------------------------
-    # Desktop: HP-i7-2660-EilteDesk-8200-SFF ATI Radeon R5 340x OEM
-    #---------------------------------------------------------------------
-    # ./machines/HP-i7-2660-EilteDesk-8200-SFF/EliteDesk-8200-configuration.nix
-
-    #---------------------------------------------------------------------
-    # Laptop: HP-i7-3615QM x 4 EliteBook Folio 9470M Intel HD Graphics 4000
-    #---------------------------------------------------------------------
-    # ./machines/HP-i7-Folio-9470M (FULL-CONFIG)/configuration.nix
-
-    #---------------------------------------------------------------------
-    # Main: HP-i7-4770 x 8 EliteDesk G1 800 SFF Nvidia GT-1030
-    #---------------------------------------------------------------------
+let
+  #---------------------------------------------------------------------
+  # I want my main rig: HP-i7-4770 x 8 EliteDesk G1 800 SFF Nvidia GT-1030
+  #---------------------------------------------------------------------
+  importfile = ( if builtins.readFile "/sys/devices/virtual/dmi/id/product_name" == "HP EliteDesk 800 G1 SFF\n" then
     ./machines/HP-i7-4770-EliteDesk-G1-800-SFF/EliteDesk-800-G1-configuration.nix
+  
+  else
 
-    #---------------------------------------------------------------------
-    # Laptop: Samsung NP530U4B i5-2467M ATI Radeon HD 7550M
-    #---------------------------------------------------------------------
-    # ./machines/Samsung-NP530U4B-configuration.nix
+    # This has to be manually symlinked per host/machine
+    /etc/nixos/manual.nix
 
-    #---------------------------------------------------------------------
-    # Laptop: Sony Vaio VPCEB13FG i3-330M x 2 Intel HD Graphics
-    #---------------------------------------------------------------------
-    # ./machines/Sony-Vaio-i3-330M (FULL-CONFIG)/configuration.nix
-
+  );
+in
+{
+  imports = [ 
+    
+    importfile 
+    
   ];
-
 }
