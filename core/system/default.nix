c6175c7af49481d0a73089d7d41d04e82b2c6b14
Tolga Erok <kingtolga@gmail.com>
2023-10-19 12:11:21 +0800

    (ツ)_/¯    Update @ 12:11 PM

--- core/system/default.nix
+++ core/system/default.nix
@@ -8,11 +8,7 @@
 
   imports = [
 
-    # ./blocker
-    # ./kernel-sysctl/SSD-28GB-default.nix       # kernel tweaks for laptop with SSD & 8GB RAM
-    # ./kernel-sysctl/SSD-default.nix               # kernel tweaks for laptop (General)
-    # ./theme    # GNOME THEME?
-    #./boot-kernel
+    # ./zram
     ./bluetooth
     ./documentation
     ./env
---
+++ core/system/zram/default.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  zramSwap = {
+
+    enable = false;
+    algorithm = "zstd"; # Use Zstandard (zstd) compression algorithm
+    #  algorithm = "lz4";  # Use LZ4 compression algorithm
+    #  algorithm = "lzo";  # Use LZO compression algorithm
+    memoryPercent = 50;  #  % of ram used for compression
+
+  };
+}
+
+
+# Zram Swap Configuration
+#zramSwap = {
+#  enable = true;     # Enable Zram Swap
+#  algorithm = "zstd";  # Use Zstandard (zstd) compression algorithm
+#  memoryPercent = 50; # Allocate 50% of RAM for Zram Swap
+#};
+
+# Zram Tmpfs Configuration
+#zramTmpfs = {
+#  enable = true;     # Enable Zram for tmpfs
+#  algorithm = "lz4";  # Use LZ4 compression algorithm
+##  memoryPercent = 25; # Allocate 25% of RAM for Zram tmpfs
+#};
+
+# Zram Extra Configuration
+#zramExtra = {
+#  enable = true;     # Enable Zram for an extra use case
+#  algorithm = "lzo";  # Use LZO compression algorithm
+#  memoryPercent = 30; # Allocate 30% of RAM for Zram extra use
+#};
+
+
+#You can copy and paste these configurations as needed.
+
+
+
+
+
