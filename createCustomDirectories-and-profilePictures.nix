0bc09fab3b534eb37e4141f667a71ff72640f431
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-10-22 17:19:41 +0800

    Update createCustomDirectories-and-profilePictures.nix
    tidy up

--- createCustomDirectories-and-profilePictures.nix
+++ createCustomDirectories-and-profilePictures.nix
@@ -5,14 +5,19 @@
 #---------------------------------------------------------------------
 # Tolga Erok
 # 22/10/23
-# My personal $HOME directory populator  
-# 
+# My personal $HOME directory populator
+#
 # ¯\_(ツ)_/¯
 #---------------------------------------------------------------------
 
 let
   setGnomeProfilePicture = ''
+    # Create profile picture directory
+    # ------------------------------------------------------------------
     mkdir -p /var/lib/AccountsService/icons
+
+    # Check if exists then copy the custom user profile pictures
+    # ------------------------------------------------------------------
     if [[ ! -h /var/lib/AccountsService/icons/tolga ]]; then
       cp /etc/nixos/SETUP/profile-pics/cool-tolga-2.png /var/lib/AccountsService/icons/tolga
       cp /etc/nixos/SETUP/profile-pics/smile.jpg /var/lib/AccountsService/icons/SOS
@@ -26,8 +31,12 @@ let
       username=$(basename "$user_home")
 
       # Skip populating directories in root and personal samba folder
+      # ------------------------------------------------------------------
+
       if [[ "$username" != "root" && "$username" != "NixOs" ]]; then
+
         # Create standard directories
+        # ------------------------------------------------------------------
         mkdir -p "$user_home/Documents"
         mkdir -p "$user_home/Downloads"
         mkdir -p "$user_home/Pictures"
@@ -39,16 +48,20 @@ let
         mkdir -p "$user_home/.ssh"
 
         # Optional: Create user-specific configuration files
+        # ------------------------------------------------------------------
         touch "$user_home/.bash_profile"
         touch "$user_home/.profile"
 
         # Get the user's primary group and set ownership
+        # ------------------------------------------------------------------
         usergroup=$(id -gn "$username")
         chown -R "$username:$usergroup" "$user_home/Documents" "$user_home/Downloads" "$user_home/Pictures" "$user_home/Music" "$user_home/Videos" "$user_home/Public" "$user_home/Templates" "$user_home/.config" "$user_home/.ssh" "$user_home/.bash_profile" "$user_home/.profile"
       fi
+
     done
 
-    # Print "User directories created" in blue
+    # Print output in blue
+    # ------------------------------------------------------------------
     echo -e "\n\e[34mUser directories created, switching back into:\e[0m $HOME\e[0m\n"
     echo -e "\e[34mUser User profile pictures set\e[0m\n"
   '';
