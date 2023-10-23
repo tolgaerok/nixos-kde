f0d996b1bfdc53be18707c7355a56da8c7182f7d
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-10-23 12:34:55 +0800

    Update enableFlatPak.nix

--- enableFlatPak.nix
+++ enableFlatPak.nix
@@ -11,19 +11,22 @@
 let
 
   enableFlatPak = ''
-    # Check flatpak, add if not present 
-    # ------------------------------------------------------------------
-    echo -e "\n\e[34mChecking flatpak repo: \e[32m¯\_👽_/¯\e[34m"
+  # Check flatpak, add if not present
+  echo -e "\n\e[34mChecking flatpak repo: \e[32m¯\_👽_/¯\e[34m"
 
-    # Enable Flatpak
-    # -----------------------------------------------------------------------------------
-    if ! flatpak remote-list | grep -q "flathub"; then
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  # Enable Flatpak
+  # -----------------------------------------------------------------------------------
+  if ! flatpak remote-list | grep -q "flathub"; then
+    if flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; then
+      echo -e "\e[34mFlatpak added and enabled: \e[32m[✔]\e[34m\n"
     else
-      echo -e "\e[34mFlatpak enabled: \e[32m[✔]\e[34m\n"      
-      sleep 1
+      echo -e "\e[31mError: \e[33mFailed to enable Flatpak \e[31m[✘]\e[0m\n"
     fi
-  '';
+  else
+    echo -e "\e[34mFlatpak passed: \e[32m[✔]\e[34m\n"
+  fi
+'';
+
 
 in {
 
