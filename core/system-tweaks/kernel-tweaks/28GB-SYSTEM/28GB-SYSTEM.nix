d9f4807f4a6572ebe190ad441e4faea9146eab4b
Tolga Erok <kingtolga@gmail.com>
2023-10-24 22:08:41 +0800

    (ツ)_/¯    Update @ 10:08 PM

--- core/system-tweaks/kernel-tweaks/28GB-SYSTEM/28GB-SYSTEM.nix
+++ core/system-tweaks/kernel-tweaks/28GB-SYSTEM/28GB-SYSTEM.nix
@@ -23,7 +23,7 @@
     "vm.dirty_bytes" = 25165824;                # Data (24 MB) modified in memory and needs to be written to disk. Adjusted for SSD.
     "vm.min_free_kbytes" = 32768;               # Minimum free memory for safety (in KB), helping prevent memory exhaustion situations. Adjusted for 28GB RAM.
     "vm.swappiness" = 5;                        # Adjust how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM. Adjusted for 28GB RAM.
-    "vm.vfs_cache_pressure" = 200;              # Adjust vfs_cache_pressure (0-1000) to manage memory used for caching filesystem objects. Adjusted for 28GB RAM.
+    "vm.vfs_cache_pressure" = 90;              # Adjust vfs_cache_pressure (0-1000) to manage memory used for caching filesystem objects. Adjusted for 28GB RAM.
 
 
 
