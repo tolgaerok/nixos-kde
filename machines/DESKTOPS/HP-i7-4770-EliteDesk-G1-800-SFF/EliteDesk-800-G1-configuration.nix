9fc51e66c029ce9a2938f6455749518021124dbb
Tolga Erok <kingtolga@gmail.com>
2023-10-27 14:08:39 +0800

    (ツ)_/¯    Update @ 02:08 PM

--- machines/DESKTOPS/HP-i7-4770-EliteDesk-G1-800-SFF/EliteDesk-800-G1-configuration.nix
+++ machines/DESKTOPS/HP-i7-4770-EliteDesk-G1-800-SFF/EliteDesk-800-G1-configuration.nix
@@ -22,21 +22,21 @@
 
     # Select your kernel
     #---------------------------------------------
-    ../../../core/system-tweaks/kernel-upgrades/latest-standard.nix         # Latest default NixOS kernel
-    # ../../core/system-tweaks/kernel-upgrades/xanmod.nix                   # Xanmod kernel
-    # ../../core/system-tweaks/kernel-upgrades/zen.nix                      # Zen kernel
+    # ../../core/system-tweaks/kernel-upgrades/xanmod.nix                     # Xanmod kernel
+    # ../../core/system-tweaks/kernel-upgrades/zen.nix                        # Zen kernel
+    ../../../core/system-tweaks/kernel-upgrades/latest-standard.nix           # Latest default NixOS kernel
 
     # Main core
     # ---------------------------------------------
     ../../../core
-    ../../../core/gpu/nvidia/nvidia-stable-opengl                           # NVIDIA with hardware acceleration (Open-GL) for GT-1030++
+    ../../../core/gpu/nvidia/nvidia-stable-opengl                             # NVIDIA with hardware acceleration (Open-GL) for GT-1030++
     ./EliteDesk-800-G1-hardware-configuration.nix
 
     # Custom System tweaks
     # ---------------------------------------------
-    ../../../core/system-tweaks/kernel-tweaks/28GB-SYSTEM/28GB-SYSTEM.nix   # Kernel tweak for 28GB
-    ../../../core/system-tweaks/storage-tweaks/SSD/SSD-tweak.nix            # SSD read & write tweaks
-    ../../../core/system-tweaks/zram/zram-28GB-SYSTEM.nix                   # Zram tweak for 28GB
+    # ../../../core/system-tweaks/zram/zram-28GB-SYSTEM.nix                   # Zram tweak for 28GB
+    ../../../core/system-tweaks/kernel-tweaks/28GB-SYSTEM/28GB-SYSTEM.nix     # Kernel tweak for 28GB
+    ../../../core/system-tweaks/storage-tweaks/SSD/SSD-tweak.nix              # SSD read & write tweaks
 
     # Users && user settings
     # ---------------------------------------------
