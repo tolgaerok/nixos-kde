16db8d8eab195fb2c56420de2dc558f1265243c0
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-10-27 21:44:52 +0800

    Update default.nix

--- core/services/earlyoom/default.nix
+++ core/services/earlyoom/default.nix
@@ -17,7 +17,7 @@
       # Technical Explanation:
       # The earlyoom service monitors system memory and intervenes when free memory drops below the specified threshold.
       # It helps prevent system slowdowns and freezes by intelligently killing less important processes to free up memory.
-      # In this configuration, it triggers when free memory is only 5% of total RAM.
+      # In this configuration, it triggers when free memory is only 15% of total RAM.
       # Adjust the freeMemThreshold value based on your system's memory usage patterns.
       
     };
