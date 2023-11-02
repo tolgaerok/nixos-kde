0b3a5f60c1548243eba31ddecc318cfe305e589a
Tolga Erok <kingtolga@gmail.com>
2023-11-02 22:19:32 +0800

    (ツ)_/¯    Update @ 10:19 PM

--- core/packages/custom-pkgs/auto-home-manager.nix
+++ core/packages/custom-pkgs/auto-home-manager.nix
@@ -21,43 +21,57 @@ let
     ##                                  Options                                   ##
     ################################################################################
 
-    host=nixos-host
-    user=user
+    host="nixos-host"
+    user="user"
 
     { set +x; } 2> /dev/null
+
+    ################################################################################
+    ##                             Helper Functions                                ##
+    ################################################################################
+
+    # Function to print formatted messages
+    msg() {
+      printf "\n\e[32m=> $1\e[0m\n\n"
+    }
+
+    # Function to confirm actions with the user
+    confirm() {
+      read -r -p "Do you want to continue? [Y/n] " response
+      case "$response" in
+        [nN][oO]|[nN]) return 1 ;;
+        *) return 0 ;;
+      esac
+    }
 
     ################################################################################
     ##                                   Script                                   ##
     ################################################################################
 
-    # Install the home-manager channel.
-    printf "\n\e[32m=> Installing the home-manager channel...\e[0m\n\n"
+    msg "Installing the home-manager channel..."
     (
         set -x
-        sudo nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+        sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
         sudo nix-channel --update
     )
 
-    # Link the root user’s home configuration.
-    printf "\n\e[32m=> Linking the root user’s home configuration...\e[0m\n\n"
+    msg "Linking the root user’s home configuration..."
     (
         set -x
         sudo mkdir -p /root/.config/nixpkgs
-        sudo ln -s /home/$user/config/confkit/Nix/root.nix /root/.config/nixpkgs/home.nix
-        sudo nix-shell '<home-manager>' -A install        
+        sudo ln -s "/home/$user/config/confkit/Nix/root.nix" /root/.config/nixpkgs/home.nix
+        sudo nix-shell '<home-manager>' -A install
     )
 
-    # Link your home configuration.
-    printf "\n\e[32m=> Linking your home configuration...\e[0m\n\n"
+    msg "Linking your home configuration..."
     (
         set -x
-        mkdir -p $HOME/.config/nixpkgs
-        ln -s ../../config/Nix/$host/$user.nix $HOME/.config/nixpkgs/home.nix
+        mkdir -p "$HOME/.config/nixpkgs"
+        ln -s "../../config/Nix/$host/$user.nix" "$HOME/.config/nixpkgs/home.nix"
         nix-shell '<home-manager>' -A install
-        
     )
 
-    printf "\n\e[32m\e[1mInstallation complete!\e[0m\n\n"
+    printf '\n\e[32m\e[1mInstallation complete!\e[0m\n\n'
     printf "You can now restart your shell."
 
   '';
@@ -65,7 +79,7 @@ let
 in {
 
   #---------------------------------------------------------------------
-  # Type: nixos-hm in terminal to execute above bash script
+  # Type: nixos-hm in terminal to execute the above bash script
   #---------------------------------------------------------------------
 
   environment.systemPackages = [ nixos-hm ];
