e20bd9b93c01eb34eb46d30da05030714b8ae02d
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-11-05 10:57:57 +0800

    Update default.nix

--- core/services/udisks2/default.nix
+++ core/services/udisks2/default.nix
@@ -11,14 +11,14 @@
 # services.udisks2.enable = true;
 
 {
+# Whether to enable udisks2, a DBus service that allows applications to query and manipulate storage devices.
+
   services = {
     udisks2 = {
-
       enable = true;
     };
 
     devmon = {
-
       enable = true;
     };
   };
