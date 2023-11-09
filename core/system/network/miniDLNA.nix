ea7d10d37c58bbdcce3b26f3cc05dc4fa30d892e
Tolga Erok <kingtolga@gmail.com>
2023-11-09 13:38:06 +0800

    (ツ)_/¯    Update @ 01:38 PM

--- core/system/network/miniDLNA.nix
+++ core/system/network/miniDLNA.nix
@@ -1,39 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  # minidlna service
-  #services.minidlna.enable = true;
-  #services.minidlna.announceInterval = 60;
-  #services.minidlna.friendlyName = "Tolgas-DLNA";
-  #services.minidlna.mediaDirs = [ "PA,/home/tolga/public/Musique/" "PV,/home/tolga/public/Videos/" ];
-
-  #DLNA
+  # DLNA service
+  # ------------------------------------------------------
   services.minidlna.enable = true;
   services.minidlna.settings = {
     friendly_name = "NixOS-DLNA";
+
+    #     https://mylinuxramblings.wordpress.com/2016/02/19/mini-how-to-installing-minidlna-in-ubuntu/
+    #    "A" for audio    (eg. media_dir=A,/var/lib/minidlna/music)
+    #    "P" for pictures (eg. media_dir=P,/var/lib/minidlna/pictures)
+    #    "V" for video    (eg. media_dir=V,/var/lib/minidlna/videos)
+    #    "PV" for pictures and video (eg. media_dir=PV,/var/lib/minidlna/digital_camera)
+
     media_dir = [
-      "PV,/home/tolga/public/Music/" # Videos files are located here
+      "PV,/home/tolga/public/Music/" # Music files are located here
       "PV,/home/tolga/public/Vids/" # Audio files are here
-      #"PV,/mnt/sambashare/"
+      "PV,/mnt/sambashare/DLNA/"
       #"PV,/mnt/DLNA/"
     ];
-    log_level = "error";
+
+    announceInterval = 5;
     inotify = "yes";
+    log_level = "error";
   };
 
   users.users.minidlna = {
     extraGroups =
-      [ "users" "samba" "wheel" "tolga"]; # so minidlna can access the files.
+      [ "users" "samba" "wheel" "tolga" ]; # so minidlna can access the files.
   };
 
-  # trick to create a directory with proper ownership
-  # note that tmpfiles are not necesserarly temporary if you don't
-  # set an expire time. Trick given on irc by someone I forgot the name..
-  systemd.tmpfiles.rules = [ "d /home/tolga/public 0777 tolga users" ];
 }
 
-#     https://mylinuxramblings.wordpress.com/2016/02/19/mini-how-to-installing-minidlna-in-ubuntu/
-#    "A" for audio    (eg. media_dir=A,/var/lib/minidlna/music)
-#    "P" for pictures (eg. media_dir=P,/var/lib/minidlna/pictures)
-#    "V" for video    (eg. media_dir=V,/var/lib/minidlna/videos)
-#    "PV" for pictures and video (eg. media_dir=PV,/var/lib/minidlna/digital_camera)
