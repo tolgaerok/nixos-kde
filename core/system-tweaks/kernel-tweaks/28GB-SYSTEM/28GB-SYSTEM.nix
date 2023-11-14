156c1cccbd6765b28b037a87fa8aed6bbb7f0118
Tolga Erok <kingtolga@gmail.com>
2023-11-14 22:51:15 +0800

    (ツ)_/¯    Update @ 10:51 PM

--- core/system-tweaks/kernel-tweaks/28GB-SYSTEM/28GB-SYSTEM.nix
+++ core/system-tweaks/kernel-tweaks/28GB-SYSTEM/28GB-SYSTEM.nix
@@ -11,19 +11,19 @@
     #---------------------------------------------------------------------
     "kernel.sysrq" = 1;                         # Enable SysRQ for rebooting the machine properly if it freezes. [Source](https://oglo.dev/tutorials/sysrq/index.html)
     "net.core.netdev_max_backlog" = 300000;      # Help prevent packet loss during high traffic periods.
-    "net.core.rmem_default" = 262144;           # Default socket receive buffer size, improve network performance & applications that use sockets. Adjusted for 28GB RAM.
+    "net.core.rmem_default" = 524288;           # Default socket receive buffer size, improve network performance & applications that use sockets. Adjusted for 28GB RAM.
     "net.core.rmem_max" = 33554432;             # Maximum socket receive buffer size, determine the amount of data that can be buffered in memory for network operations. Adjusted for 28GB RAM.
-    "net.core.wmem_default" = 262144;           # Default socket send buffer size, improve network performance & applications that use sockets. Adjusted for 28GB RAM.
+    "net.core.wmem_default" = 524288;           # Default socket send buffer size, improve network performance & applications that use sockets. Adjusted for 28GB RAM.
     "net.core.wmem_max" = 33554432;             # Maximum socket send buffer size, determine the amount of data that can be buffered in memory for network operations. Adjusted for 28GB RAM.
-    "net.ipv4.ipfrag_high_threshold" = 5242880; # Reduce the chances of fragmentation. Adjusted for SSD.
+    "net.ipv4.ipfrag_high_threshold" = 6291456; # Reduce the chances of fragmentation. Adjusted for SSD.
     "net.ipv4.tcp_keepalive_intvl" = 30;        # TCP keepalive interval between probes to detect if a connection is still alive.
     "net.ipv4.tcp_keepalive_probes" = 5;        # TCP keepalive probes to detect if a connection is still alive.
     "net.ipv4.tcp_keepalive_time" = 300;        # TCP keepalive interval in seconds to detect if a connection is still alive.
     "vm.dirty_background_bytes" = 474217728;     # 128 MB + 300 MB + 400 MB = 828 MB (rounded to 474217728)
     "vm.dirty_bytes" = 742653184;                # 384 MB + 300 MB + 400 MB = 1084 MB (rounded to 742653184)
-    "vm.min_free_kbytes" = 32768;               # Minimum free memory for safety (in KB), helping prevent memory exhaustion situations. Adjusted for 28GB RAM.
-    "vm.swappiness" = 5;                        # Adjust how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM. Adjusted for 28GB RAM.
-    "vm.vfs_cache_pressure" = 95;               # Adjust vfs_cache_pressure (0-1000) to manage memory used for caching filesystem objects. Adjusted for 28GB RAM.
+    "vm.min_free_kbytes" = 65536;               # Minimum free memory for safety (in KB), helping prevent memory exhaustion situations. Adjusted for 28GB RAM.
+    "vm.swappiness" = 10;                        # Adjust how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM. Adjusted for 28GB RAM.
+    "vm.vfs_cache_pressure" = 150;               # Adjust vfs_cache_pressure (0-1000) to manage memory used for caching filesystem objects. Adjusted for 28GB RAM.
 
 
 
