127c7aa331563b8a7686ab3b4040ebab0aa04ad0
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-12-02 11:26:59 +0800

    Update default.nix

--- core/system/network/default.nix
+++ core/system/network/default.nix
@@ -54,3 +54,26 @@
 
 }
 
+# wireless = {
+      # via wpa_supplicant.
+#      enable = false;
+#      iwd = {
+#        enable = false;
+ #       settings = {
+#          Network = {
+#            EnableIPv6 = true;
+#            RoutePriorityOffset = 300;
+#          };
+ #         Settings = {
+  #          AutoConnect = true;
+ #         };
+ #       };
+#      };
+#    };
+    ## Configure network proxy if necessary.
+    # proxy = {
+    #   default = "http://user:password@proxy:port/";
+    #   noProxy = "127.0.0.1,localhost,internal.domain";
+    # };
+#  };
+
