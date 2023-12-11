780f7028ae4e7fb22ef1fea249de6ba75ff7f8a9
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-12-11 22:17:40 +0800

    Update default.nix

--- core/services/udev/default.nix
+++ core/services/udev/default.nix
@@ -17,6 +17,7 @@
       extraRules = ''
         KERNEL=="rtc0", GROUP="audio"
         KERNEL=="hpet", GROUP="audio"
+        ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="ext4", ATTR{../queue/scheduler}="none"
       '';
 
     };
