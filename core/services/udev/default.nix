b5b2eda7941535daf9f7d68015492e608a1a8600
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-12-12 20:20:45 +0800

    Update default.nix

--- core/services/udev/default.nix
+++ core/services/udev/default.nix
@@ -13,11 +13,14 @@
       enable = true;
 
       # enable high precision timers if they exist && set I/O scheduler to NONE for ssd/nvme
+      # autosuspend USB devices && autosuspend PCI devices
       # (https://gentoostudio.org/?page_id=420)
       extraRules = ''
         KERNEL=="rtc0", GROUP="audio"
         KERNEL=="hpet", GROUP="audio"
-        ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="ext4", ATTR{../queue/scheduler}="none"
+        ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="ext4", ATTR{../queue/scheduler}="mq-deadline"
+        ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"
+        ACTION=="add", SUBSYSTEM=="pci", TEST=="power/control", ATTR{power/control}="auto"
       '';
 
     };
