057ff62382916f54e5432be048775b5423f93053
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-12-25 21:44:49 +0800

    Update efi.nix

--- core/boot/efi/efi.nix
+++ core/boot/efi/efi.nix
@@ -17,7 +17,8 @@
       systemd-boot.enable = true;         # Activates the systemd-boot bootloader.
     };
 
-    initrd.systemd.enable = true;         # Enables systemd services in the initial ramdisk (initrd).
+    initrd.systemd.enable = true;         # Enables systemd services in the initial ramdisk (initrd).    
+    initrd.verbose = false;               # silent boot
     plymouth.enable = true;               # Activates the Plymouth boot splash screen.
     plymouth.theme = "breeze";            # Sets the Plymouth theme to "breeze."        
   };
