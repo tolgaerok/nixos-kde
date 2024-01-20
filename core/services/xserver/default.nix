4cc434d3a4eb3ea5d9bcd675ab8cef0bbfbe24ec
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2024-01-20 21:10:52 +0800

    Update default.nix

--- core/services/xserver/default.nix
+++ core/services/xserver/default.nix
@@ -7,7 +7,6 @@
   #---------------------------------------------------------------------
 
   imports = [
-
     # Choose DE, still under review
 
     # ./cinnamon/cinnamon.nix
@@ -15,28 +14,23 @@
     # ./hyperland/hyperland.nix
     # ./kde/kde.nix
     # ./sway/sway.nix
-
   ];
 
   services.xserver = {
     enable = true;
-    
+    desktopManager.wallpaper.mode = "scale";
     # scaling of fonts and graphical elements on the screen
     dpi = 98;
-
     libinput.enable =
       true; # Enable touchpad support (enabled default in most desktopManager).
 
     desktopManager = {
-
       plasma5.enable = true;
     };
-
     displayManager = {
       sddm = {
         enable = true;
         autoNumlock = true;
-
       };
     };
 
@@ -44,15 +38,13 @@
     # Vidoe settings that go hand-in-hand with opengl
     # ---------------------------------------------------------------------
     videoDrivers = [
-
       "fbdev" # The fbdev (Framebuffer Device) driver is a generic framebuffer driver that provides access to the frame buffer of the display hardware.
       # "modesetting"     # The modesetting driver is a generic driver for modern video hardware that relies on kernel modesetting (KMS) to set the display modes and manage resolution and refresh rate.
       # "amdgpu"          # This is the open-source kernel driver for AMD graphics cards. It's part of the AMDGPU driver stack and provides support for newer AMD GPUs.
       # "nouveau"         # Nouveau is an open-source driver for NVIDIA graphics cards. It aims to provide support for NVIDIA GPUs and is an alternative to the proprietary NVIDIA driver
       # "radeon"          # The Radeon driver is an open-source driver for AMD Radeon graphics cards. It provides support for older AMD GPUs.
-
+      "nvidia"
     ];
-
   };
 
   # Old notes to self
